522bd60affe891d4a3da49d14df01c6382c42c4a
Pietro Vischia <pietro.vischia@gmail.com>
2016-12-02 16:57:05 +0100

    Other trees
    Must really make it flexible

--- TTHAnalysis/python/plotter/susy-interface/cmds/tau-ewkino/chunkCombiner.sh
+++ TTHAnalysis/python/plotter/susy-interface/cmds/tau-ewkino/chunkCombiner.sh
@@ -18,7 +18,10 @@
 # 
 # cd -
 
-cd /pool/ciencias/HeppyTrees/RA7/estructura/testbtag/leptonBuilderEWK
+#cd /pool/ciencias/HeppyTrees/RA7/estructura/testbtag/leptonBuilderEWK
+
+
+cd /pool/ciencias/HeppyTrees/RA7/estructura/testbtag/leptonJetReCleanerSusyEWK3L
 
 sh $CMSSW_BASE/src/CMGTools/TTHAnalysis/macros/leptons/friendChunkAdd.sh evVarFriend
 
