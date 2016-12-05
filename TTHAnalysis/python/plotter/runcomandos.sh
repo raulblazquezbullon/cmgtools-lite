1cd324736c43e3bc060811a9321ed194c609fba6
GonzalezFJR <juan.rodrigo.gonzalez.fernandez@cern.ch>
2016-12-05 09:53:26 +0100

    Updated

--- TTHAnalysis/python/plotter/runcomandos.sh
+++
@@ -1,8 +0,0 @@
-# sh comandoPlotsCuts.sh "plot1, plot2" "-E corte1 -E corte2"
-# j0 -E mtminse100 -E HH
-sh comandoPlotCut.sh "MET, MET_log" "-E j0 -E mtminse100 -E noHH"
-sh comandoPlotCut.sh "MET, MET_log" "-E j0 -E mtminse100 -E HH"
-sh comandoPlotCut.sh "MET, MET_log" "-E j0 -E mtmin100"
-sh comandoPlotCut.sh "MET, MET_log" "-E j1 -E mtminse100 -E noHH"
-sh comandoPlotCut.sh "MET, MET_log" "-E j1 -E mtminse100 -E HH"
-sh comandoPlotCut.sh "MET, MET_log" "-E j1 -E mtmin100"
