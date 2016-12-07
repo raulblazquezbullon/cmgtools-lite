d8e95de64d56ad4c0d5e268739e5af899e961fa1
Pietro Vischia <pietro.vischia@gmail.com>
2016-12-07 16:10:03 +0100

    Made chunkDealer flexible. Bugfix in friendChunkCheck

--- TTHAnalysis/python/plotter/susy-interface/cmds/tau-ewkino/chunkDealer.sh
+++ TTHAnalysis/python/plotter/susy-interface/cmds/tau-ewkino/chunkDealer.sh
@@ -1,26 +1,34 @@
 #!/bin/bash
 
 INPUT="$1"
-PREFIX="$2"
+ACTION="$2"
+PREFIX="$3"
 
-if [ "${INPUT}" == "help"]; then
-    echo "USAGE: ${0} chunksFolder prefix"
+if [ "${INPUT}" == "help" ]; then
+    echo "USAGE: ${0} chunksFolder [check|merge] prefix"
     echo "Default prefix: evVarFriend"
     exit 0
+fi
+
+CMD="$CMSSW_BASE/src/CMGTools/TTHAnalysis/macros/leptons/friendChunkAdd.sh ${PREFIX}"
+
+if [ "${ACTION}" == "check" ]; then
+    CMD="$CMSSW_BASE/src/CMGTools/TTHAnalysis/macros/leptons/friendChunkCheck.sh -z ${INPUT}"
 fi
 
 if [ "${PREFIX}" == "" ]; then
     PREFIX="evVarFriend"
 fi
 
-
-
 cd ${INPUT}
 
-sh $CMSSW_BASE/src/CMGTools/TTHAnalysis/macros/leptons/friendChunkAdd.sh ${PREFIX}
+sh ${CMD}
 
-mkdir chunks
-mv *.chunk*root chunks/
+if [ "${ACTION}" == "merge" ]; then
+    mkdir chunks
+    mv *.chunk*root chunks/
+fi
 
 cd -
                            
+exit 0
