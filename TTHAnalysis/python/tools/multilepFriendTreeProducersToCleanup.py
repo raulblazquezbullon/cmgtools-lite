4d8ebe4552493b3a396e656a967f0a50c7b09edb
cheidegg <cheidegg@student.ethz.ch>
2016-12-12 15:30:10 +0100

    adding cleanTausWithLoose for EWK2L friend tree

--- TTHAnalysis/python/tools/multilepFriendTreeProducersToCleanup.py
+++ TTHAnalysis/python/tools/multilepFriendTreeProducersToCleanup.py
@@ -117,6 +117,7 @@ MODULES.append( ('leptonJetReCleanerSusyEWK2L', lambda : LeptonJetReCleaner("Rec
                    looseTau = lambda tau: _susyEWK_tauId_CBloose(tau), # used in cleaning
                    tightTau = lambda tau: _susyEWK_tauId_CBtight(tau), # on top of loose
                    cleanJetsWithTaus = True,
+                   cleanTausWithLoose = True,
                    doVetoZ = True,
                    doVetoLMf = True,
                    doVetoLMt = True,
