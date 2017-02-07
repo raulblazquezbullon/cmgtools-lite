605dd238b4afc222adaa337772365126399aec72
cheidegg <cheidegg@student.ethz.ch>
2017-02-07 15:53:18 +0100

    email functionality

--- TTHAnalysis/python/plotter/susy-interface/scripts/psibatch_runner.sh
+++ TTHAnalysis/python/plotter/susy-interface/scripts/psibatch_runner.sh
@@ -7,4 +7,7 @@ cd [WORK]
 [PLACEHOLDER]
 echo "Job for SUSY-Interface task [INST] is done."
 echo "Log files can be found at [WORK]/susy-interface/tmp/[INST]/log"
+if [ ! -z "[EMAIL]" ]; then
+  echo "Job for SUSY-Interface task [INST] is done." | mail -s "SUSY-Interface JobControl" [EMAIL]
+fi
 touch [JOBDIR]/[JOBID]
