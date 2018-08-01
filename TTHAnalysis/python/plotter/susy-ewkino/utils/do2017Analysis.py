ed23a5025a6eb22609bb3117fcb01c9748a7ac64
Carlos Erice <cericecid@gmail.com>
2018-08-01 12:42:35 +0200

    Friend Tree producing save commands

--- TTHAnalysis/python/plotter/susy-ewkino/utils/do2017Analysis.py
+++ TTHAnalysis/python/plotter/susy-ewkino/utils/do2017Analysis.py
@@ -9,7 +9,7 @@ pr.add_option("--pdir", dest = "outname", type="string", default = "~/www/EWKino
 pr.add_option("--idir", dest = "inname", type="string", default = None, help = "Input (folder) name")
 pr.add_option("--cut", dest = "cut", type="string", default = None, help = "Input cutfile")
 pr.add_option("--mca", dest = "mca", type="string", default = None, help = "Input mcafile")
-pr.add_option("-j", dest = "jobs", type="int", default = None, help = "Number of simultaneous jobs for multithreaded cases")
+pr.add_option("-j", dest = "jobs", type="int", default = None, help = "Number of simultaneous jobs for multithreaded procedures")
 
 #Output formats
 (options, args) = pr.parse_args()
@@ -45,15 +45,30 @@ if doWhat == "Fits":
         print " python fitVar.py --file ../../../../data/WZ_MC/likelihood_2017.root --histo mtW3l_prompt_WZ --lX \'M_{T}^{W} [GeV]\' --lY 'Events' --fit mtW --pdir %s/mtw_fit"%options.outname
 
 ###FriendTree producer
-
 if "friend_" in doWhat:
-    baseCommand = "python prepareEventVariablesFriendTree.py  [INTREES] [OUTTREES] --tra2 --tree treeProducerSusyMultilepton --vector -T sf  -m vtxWeight -m vtxWeightUp -m vtxWeightDown -N 100000 -I CMGTools.TTHAnalysis.tools.multilepFriendTreeProducersToCleanup --onlyMC"
-    ### Pile-up reweighting. Nominal (62.9) +-5% files already in ../data/pileup/ folder
-    if "PU" in doWhat or "pileup" in doWhat:
-        print "Pile-up friend tree producer, from $CMSSW/src/CMGTools/TTHAnalysis/macros:"
+    baseCommand = "python prepareEventVariablesFriendTree.py  [INTREES] [OUTTREES] --tra2 --tree treeProducerSusyMultilepton --vector -T sf   -N 100000 -I CMGTools.TTHAnalysis.tools.multilepFriendTreeProducersToCleanup --onlyMC"
+    print "Friend tree producer: cd $CMSSW/src/CMGTools/TTHAnalysis/macros/"
+    if "PU" in doWhat or "PileUp" in doWhat:
+        ### Pile-up reweighting. Nominal (62.9) +-5% files already in ../data/pileup/ folder
         print "The script runs all trees in %s by default unless a specific dataset is given by -d"%options.inname
-        print baseCommand.replace("[INTREES]", options.inname).replace("[OUTTREES]", options.outname)
+        print baseCommand.replace("[INTREES]", options.inname).replace("[OUTTREES]", options.outname).replace("[MODULE]", "-m vtxWeight -m vtxWeightUp -m vtxWeightDown")
 
+    elif "ReCleaner" in doWhat or "RC" in doWhat:
+        ### Standard 3l recleaner
+        print "The script runs all trees in %s by default unless a specific dataset is given by -d"%options.inname
+        print baseCommand.replace("[INTREES]", options.inname).replace("[OUTTREES]", options.outname).replace("[MODULE]", "-m leptonReCleanerSUSYEWK3L")
+
+    elif "Builder" in doWhat or "B" in doWhat:
+        ### Standard 3l recleaner
+        print "The script runs all trees in %s by default unless a specific dataset is given by -d"%options.inname
+        print baseCommand.replace("[INTREES]", options.inname).replace("[OUTTREES]", options.outname).replace("[MODULE]", "-m leptonBuilderEWK --F sf/t pool/ciencias/HeppyTrees/RA7/estructura_MC_Jul2017_EWKINO_skim3LepGood/1_reclEWK3L/evVarFriend_{cname}.root")
+
+    elif "bTag" in doWhat or "BW" in doWhat:
+        ### Standard 3l recleaner
+        print "The script runs all trees in %s by default unless a specific dataset is given by -d"%options.inname
+        print baseCommand.replace("[INTREES]", options.inname).replace("[OUTTREES]", options.outname).replace("[MODULE]", "-m eventBTagWeightDeepCSV --F sf/t pool/ciencias/HeppyTrees/RA7/estructura_MC_Jul2017_EWKINO_skim3LepGood/1_reclEWK3L/evVarFriend_{cname}.root")
+
+###Tree Skimming
 if doWhat == "skim":
         mca = "susy-ewkino/utils/mca_3l_skim.txt" if not options.mca, else options.mca
         cut = "susy-ewkino/utils/cuts_3l_skim.txt" if not options.cut, else options.cut
