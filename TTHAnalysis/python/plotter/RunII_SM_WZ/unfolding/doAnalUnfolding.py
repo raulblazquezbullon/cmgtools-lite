4a94f2e3b007522b5bdcb50c4ed8c4b4477465da
Pietro Vischia <pietro.vischia@gmail.com>
2019-10-07 11:17:13 +0200

    Forgot to update executable path

--- TTHAnalysis/python/plotter/RunII_SM_WZ/unfolding/doAnalUnfolding.py
+++ TTHAnalysis/python/plotter/RunII_SM_WZ/unfolding/doAnalUnfolding.py
@@ -206,6 +206,7 @@ class Steer:
     def get_list_of_jobs(self, base):
     
         ret=[]
+        executable='RunII_SM_WZ/unfolding/unfold.py'
 
         #for charge in [ '', '_plus', '_minus' ]:
         #for charge in [ '_plus', '_minus' ]:
@@ -213,29 +214,29 @@ class Steer:
             for fs in [ 'incl', 'eee', 'eem', 'mme', 'mmm']:
                 if not base:
                     # No bias, area constraint     
-                    ret.append('python wzsm/unfold.py -i {outputdir} -f {fs} --charge {charge}  -b 0 -a -o unfold_nobias/{fs}{charge}/data -c common/WZSR.input.root -r >& logs/nobias_data_{fs}.log'.format(outputdir=self.outputDir,fs=fs,charge=charge))
-                    ret.append('python wzsm/unfold.py -i {outputdir} -f {fs} --charge {charge}  -b 0 -a -o unfold_nobias/{fs}{charge}/mcclosure -c common/WZSR.input.root -r --closure >& logs/nobias_mcclosure_{fs}.log'.format(outputdir=self.outputDir,fs=fs,charge=charge))
+                    ret.append('python {executable} -i {outputdir} -f {fs} --charge {charge}  -b 0 -a -o unfold_nobias/{fs}{charge}/data -c common/WZSR.input.root -r >& logs/nobias_data_{fs}.log'.format(outputdir=self.outputDir,executable=executable,fs=fs,charge=charge))
+                    ret.append('python {executable} -i {outputdir} -f {fs} --charge {charge}  -b 0 -a -o unfold_nobias/{fs}{charge}/mcclosure -c common/WZSR.input.root -r --closure >& logs/nobias_mcclosure_{fs}.log'.format(outputdir=self.outputDir,executable=executable,fs=fs,charge=charge))
 
                     # No bias, no area constraint
-                    ret.append('python wzsm/unfold.py -i {outputdir} -f {fs} --charge {charge}  -b 0 -o unfold_nobias_noconstraintarea/{fs}{charge}/data -c common/WZSR.input.root -r >& logs/nobias_noconstraintarea_data_{fs}.log'.format(outputdir=self.outputDir,fs=fs,charge=charge))
-                    ret.append('python wzsm/unfold.py -i {outputdir} -f {fs} --charge {charge}  -b 0 -o unfold_nobias_noconstraintarea/{fs}{charge}/mcclosure -c common/WZSR.input.root -r --closure >& logs/nobias_noconstraintarea_mcclosure_{fs}.log'.format(outputdir=self.outputDir,fs=fs,charge=charge))
+                    ret.append('python {executable} -i {outputdir} -f {fs} --charge {charge}  -b 0 -o unfold_nobias_noconstraintarea/{fs}{charge}/data -c common/WZSR.input.root -r >& logs/nobias_noconstraintarea_data_{fs}.log'.format(outputdir=self.outputDir,executable=executable,fs=fs,charge=charge))
+                    ret.append('python {executable} -i {outputdir} -f {fs} --charge {charge}  -b 0 -o unfold_nobias_noconstraintarea/{fs}{charge}/mcclosure -c common/WZSR.input.root -r --closure >& logs/nobias_noconstraintarea_mcclosure_{fs}.log'.format(outputdir=self.outputDir,executable=executable,fs=fs,charge=charge))
             
                     # Bias 1, area constraint
-                    ret.append('python wzsm/unfold.py -i {outputdir} -f {fs} --charge {charge}  -b 1.0 -a -o unfold_1p0bias/{fs}{charge}/data -c common/WZSR.input.root -r >& logs/1p0bias_data_{fs}.log'.format(outputdir=self.outputDir,fs=fs,charge=charge))
-                    ret.append('python wzsm/unfold.py -i {outputdir} -f {fs} --charge {charge}  -b 1.0 -a -o unfold_1p0bias/{fs}{charge}/mcclosure -c common/WZSR.input.root -r --closure >& logs/1p0bias_mcclosure_{fs}.log'.format(outputdir=self.outputDir,fs=fs,charge=charge))
+                    ret.append('python {executable} -i {outputdir} -f {fs} --charge {charge}  -b 1.0 -a -o unfold_1p0bias/{fs}{charge}/data -c common/WZSR.input.root -r >& logs/1p0bias_data_{fs}.log'.format(outputdir=self.outputDir,executable=executable,fs=fs,charge=charge))
+                    ret.append('python {executable} -i {outputdir} -f {fs} --charge {charge}  -b 1.0 -a -o unfold_1p0bias/{fs}{charge}/mcclosure -c common/WZSR.input.root -r --closure >& logs/1p0bias_mcclosure_{fs}.log'.format(outputdir=self.outputDir,executable=executable,fs=fs,charge=charge))
                 
                     # Bias 1, no area constraint
-                    ret.append('python wzsm/unfold.py -i {outputdir} -f {fs} --charge {charge}  -b 1.0 -o unfold_1p0bias_noconstraintarea/{fs}{charge}/data -c common/WZSR.input.root -r >& logs/1p0bias_noconstraintarea_data_{fs}.log'.format(outputdir=self.outputDir,fs=fs,charge=charge))
-                    ret.append('python wzsm/unfold.py -i {outputdir} -f {fs} --charge {charge}  -b 1.0 -o unfold_1p0bias_noconstraintarea/{fs}{charge}/mcclosure -c common/WZSR.input.root -r --closure >& logs/1p0bias_noconstraintarea_mcclosure_{fs}.log'.format(outputdir=self.outputDir,fs=fs,charge=charge))
+                    ret.append('python {executable} -i {outputdir} -f {fs} --charge {charge}  -b 1.0 -o unfold_1p0bias_noconstraintarea/{fs}{charge}/data -c common/WZSR.input.root -r >& logs/1p0bias_noconstraintarea_data_{fs}.log'.format(outputdir=self.outputDir,executable=executable,fs=fs,charge=charge))
+                    ret.append('python {executable} -i {outputdir} -f {fs} --charge {charge}  -b 1.0 -o unfold_1p0bias_noconstraintarea/{fs}{charge}/mcclosure -c common/WZSR.input.root -r --closure >& logs/1p0bias_noconstraintarea_mcclosure_{fs}.log'.format(outputdir=self.outputDir,executable=executable,fs=fs,charge=charge))
         
                     # Bias 1.13, no area constraint
-                    ret.append('python wzsm/unfold.py -i {outputdir} -f {fs} --charge {charge}  -b 1.13 -o unfold_nnlobias_noconstraintarea/{fs}{charge}/data -c common/WZSR.input.root -r >& logs/nnlobias_noconstraintarea_data_{fs}.log'.format(outputdir=self.outputDir,fs=fs,charge=charge))
-                    ret.append('python wzsm/unfold.py -i {outputdir} -f {fs} --charge {charge}  -b 1.13 -o unfold_nnlobias_noconstraintarea/{fs}{charge}/mcclosure -c common/WZSR.input.root -r --closure >& logs/nnlobias_noconstraintarea_mcclosure_{fs}.log'.format(outputdir=self.outputDir,fs=fs,charge=charge))
+                    ret.append('python {executable} -i {outputdir} -f {fs} --charge {charge}  -b 1.13 -o unfold_nnlobias_noconstraintarea/{fs}{charge}/data -c common/WZSR.input.root -r >& logs/nnlobias_noconstraintarea_data_{fs}.log'.format(outputdir=self.outputDir,executable=executable,fs=fs,charge=charge))
+                    ret.append('python {executable} -i {outputdir} -f {fs} --charge {charge}  -b 1.13 -o unfold_nnlobias_noconstraintarea/{fs}{charge}/mcclosure -c common/WZSR.input.root -r --closure >& logs/nnlobias_noconstraintarea_mcclosure_{fs}.log'.format(outputdir=self.outputDir,executable=executable,fs=fs,charge=charge))
 
                 else:
                     # Bias 1.13, area constraint
-                    ret.append('python wzsm/unfold.py -i {outputdir} -f {fs} --charge {charge}  -b 1.13 -a -o unfold_nnlobias/{fs}{charge}/data -c common/WZSR.input.root -r >& logs/nnlobias_data_{fs}.log'.format(outputdir=self.outputDir,fs=fs,charge=charge))
-                    ret.append('python wzsm/unfold.py -i {outputdir} -f {fs} --charge {charge}  -b 1.13 -a -o unfold_nnlobias/{fs}{charge}/mcclosure -c common/WZSR.input.root -r --closure >& logs/nnlobias_mcclosure_{fs}.log'.format(outputdir=self.outputDir,fs=fs,charge=charge))
+                    ret.append('python {executable} -i {outputdir} -f {fs} --charge {charge}  -b 1.13 -a -o unfold_nnlobias/{fs}{charge}/data -c common/WZSR.input.root -r >& logs/nnlobias_data_{fs}.log'.format(outputdir=self.outputDir,executable=executable,fs=fs,charge=charge))
+                    ret.append('python {executable} -i {outputdir} -f {fs} --charge {charge}  -b 1.13 -a -o unfold_nnlobias/{fs}{charge}/mcclosure -c common/WZSR.input.root -r --closure >& logs/nnlobias_mcclosure_{fs}.log'.format(outputdir=self.outputDir,executable=executable,fs=fs,charge=charge))
             
                     
         return ret
