58f3a6b7d2efe45372cf2c887451041aef274f2d
Barbara Alvarez <balvarez@cern.ch>
2020-04-13 11:55:12 +0200

    adding new files for TnP ntuple production

---
+++ TTHAnalysis/cfg/run_TnP_MC.py
@@ -0,0 +1,226 @@
+import re, os, sys
+from CMGTools.RootTools.samples.configTools import printSummary, mergeExtensions, doTestN, configureSplittingFromTime, cropToLumi
+from CMGTools.RootTools.samples.autoAAAconfig import autoAAA
+from PhysicsTools.HeppyCore.framework.heppy_loop import getHeppyOption
+
+from CMGTools.RootTools.samples.ComponentCreator import ComponentCreator
+kreator = ComponentCreator()
+def byCompName(components, regexps):
+    return [ c for c in components if any(re.match(r, c.name) for r in regexps) ]
+
+year = int(getHeppyOption("year", "2017"))
+analysis = getHeppyOption("analysis", "main")
+preprocessor = getHeppyOption("nanoPreProcessor")
+
+# Samples
+from CMGTools.RootTools.samples.samples_5TeV_RunIIpp5Spring18MiniAODv1 import samples as mcSamples_
+from CMGTools.RootTools.samples.samples_5TeV_DATA2017_NanoAOD import dataSamples_Run2017G as allData
+
+#mcSamples_=[]
+##mcSamples_, _ = mergeExtensions(mcSamples_)
+
+from CMGTools.TTHAnalysis.tools.nanoAOD.WZ_modules import triggerGroups_dict
+
+flavor = getHeppyOption("flavor")
+DatasetsAndTriggers = []
+if analysis == "main":
+    mcSamples =    byCompName(mcSamples_, ["%s(|_PS)$"%dset for dset in [
+##        # single boson
+##        "WJetsToLNu", "W0JetsToLNu", "W1JetsToLNu", "W2JetsToLNu", "W3JetsToLNu", 
+        "DYJetsToLL_M10to50", "DYJetsToLL_M50", "DY0JetsToLL_M50", "DY1JetsToLL_M50", "DY2JetsToLL_M50", "DY3JetsToLL_M50" 
+##        # ttbar + single top + tW
+##        "TT", "TT_PS", "TT_hdampDown", "TT_hdampUp","TT_mtop166p5", "TT_mtop178p5", "TT_TuneCP5Up", "TT_TuneCP5Down",
+##        "T_tWch_noFullyHad", "TBar_tWch_noFullyHad","T_tWch_noFullyHad_PS", "TBar_tWch_noFullyHad_PS",
+##        # diboson + DPS + WWss
+##        "WWTo2L2Nu", "WZTo3LNu", "ZZTo4L", "ZZTo2L2Nu",
+    ]])
+
+    if flavor == 'Muon':
+        print 'Muon selection'
+        DatasetsAndTriggers.append( ("DoubleMuon", triggerGroups_dict["Trigger_5TeV_2m"][year]) )
+        DatasetsAndTriggers.append( ("SingleMuon", triggerGroups_dict["Trigger_5TeV_1m"][year]) )
+    elif flavor == 'Electron':
+        print 'Electron selection'
+        DatasetsAndTriggers.append( ("HighEGJet", triggerGroups_dict["Trigger_5TeV_1e"][year] + triggerGroups_dict["Trigger_5TeV_2e"][year]) )
+        
+# make MC
+mcTriggers = sum((trigs for (pd,trigs) in DatasetsAndTriggers if trigs), [])
+if getHeppyOption('applyTriggersInMC'):
+    for comp in mcSamples:
+        comp.triggers = mcTriggers
+
+# make data
+dataSamples = []; vetoTriggers = []
+for pd, trigs in DatasetsAndTriggers:
+    if not trigs: continue
+    for comp in byCompName(allData, [pd]):
+        comp.triggers = trigs[:]
+        comp.vetoTriggers = vetoTriggers[:]
+        dataSamples.append(comp)
+    vetoTriggers += trigs[:]
+
+selectedComponents = mcSamples
+print selectedComponents
+if getHeppyOption('selectComponents'):
+    if getHeppyOption('selectComponents')=='MC':
+        selectedComponents = mcSamples
+    elif getHeppyOption('selectComponents')=='DATA':
+        selectedComponents = dataSamples
+    else:
+        selectedComponents = byCompName(selectedComponents, getHeppyOption('selectComponents').split(","))
+if year==2018:
+    configureSplittingFromTime(byCompName(mcSamples,['^(?!(TTJets_Single|T_|TBar_)).*']),150 if preprocessor else 10,12)
+    configureSplittingFromTime(byCompName(mcSamples,['^(TTJets_Single|T_|TBar_).*']),70 if preprocessor else 10,12)
+    configureSplittingFromTime(dataSamples,50 if preprocessor else 5,12)
+else: # rerunning deepFlavor can take up to twice the time, some samples take up to 400 ms per event
+    configureSplittingFromTime(byCompName(mcSamples,['^(?!(TTJets_Single|T_|TBar_)).*']),300 if preprocessor else 10,12)
+    configureSplittingFromTime(byCompName(mcSamples,['^(TTJets_Single|T_|TBar_).*']),150 if preprocessor else 10,12)
+    configureSplittingFromTime(dataSamples,100 if preprocessor else 5,12)
+    configureSplittingFromTime(byCompName(dataSamples,['Single']),50 if preprocessor else 5,12)
+selectedComponents, _ = mergeExtensions(selectedComponents)
+
+# create and set preprocessor if requested
+if preprocessor:
+    from CMGTools.Production.nanoAODPreprocessor import nanoAODPreprocessor
+    preproc_cfg = {2016: ("mc94X2016","data94X2016"),
+                   2017: ("mc94Xv2","data94Xv2"),
+                   2018: ("mc102X","data102X_ABC","data102X_D")}
+    preproc_cmsswArea = "/afs/cern.ch/user/p/peruzzi/work/cmgtools_tth/CMSSW_10_2_16_UL"
+    preproc_mc = nanoAODPreprocessor(cfg='%s/src/PhysicsTools/NanoAOD/test/%s_NANO.py'%(preproc_cmsswArea,preproc_cfg[year][0]),cmsswArea=preproc_cmsswArea,keepOutput=True)
+    if year==2018:
+        preproc_data_ABC = nanoAODPreprocessor(cfg='%s/src/PhysicsTools/NanoAOD/test/%s_NANO.py'%(preproc_cmsswArea,preproc_cfg[year][1]),cmsswArea=preproc_cmsswArea,keepOutput=True,injectTriggerFilter=True,injectJSON=True)
+        preproc_data_D = nanoAODPreprocessor(cfg='%s/src/PhysicsTools/NanoAOD/test/%s_NANO.py'%(preproc_cmsswArea,preproc_cfg[year][2]),cmsswArea=preproc_cmsswArea,keepOutput=True,injectTriggerFilter=True,injectJSON=True)
+        for comp in selectedComponents:
+            if comp.isData:
+                comp.preprocessor = preproc_data_D if '2018D' in comp.name else preproc_data_ABC
+            else:
+                comp.preprocessor = preproc_mc
+    else:
+        preproc_data = nanoAODPreprocessor(cfg='%s/src/PhysicsTools/NanoAOD/test/%s_NANO.py'%(preproc_cmsswArea,preproc_cfg[year][1]),cmsswArea=preproc_cmsswArea,keepOutput=True,injectTriggerFilter=True,injectJSON=True)
+        for comp in selectedComponents:
+            comp.preprocessor = preproc_data if comp.isData else preproc_mc
+    if year==2017:
+        preproc_mcv1 = nanoAODPreprocessor(cfg='%s/src/PhysicsTools/NanoAOD/test/%s_NANO.py'%(preproc_cmsswArea,"mc94Xv1"),cmsswArea=preproc_cmsswArea,keepOutput=True)
+        for comp in selectedComponents:
+            if comp.isMC and "Fall17MiniAODv2" not in comp.dataset:
+                print "Warning: %s is MiniAOD v1, dataset %s" % (comp.name, comp.dataset)
+                comp.preprocessor = preproc_mcv1
+    if getHeppyOption("fast"):
+        for comp in selectedComponents:
+            comp.preprocessor = comp.preprocessor.clone(cfgHasFilter = True, inlineCustomize = ("""
+process.selectEl = cms.EDFilter("PATElectronRefSelector",
+    src = cms.InputTag("slimmedElectrons"),
+    cut = cms.string("pt > 4.5 && miniPFIsolation.chargedHadronIso < 0.45*pt && abs(dB('PV3D')) < 8*edB('PV3D')"),
+    filter = cms.bool(False),
+)
+process.selectMu = cms.EDFilter("PATMuonRefSelector",
+    src = cms.InputTag("slimmedMuons"),
+    cut = cms.string("pt > 3 && miniPFIsolation.chargedHadronIso < 0.45*pt && abs(dB('PV3D')) < 8*edB('PV3D')"),
+    filter = cms.bool(False),
+)
+process.skimNLeps = cms.EDFilter("PATLeptonCountFilter",
+    electronSource = cms.InputTag("selectEl"),
+    muonSource = cms.InputTag("selectMu"),
+    tauSource = cms.InputTag(""),
+    countElectrons = cms.bool(True),
+    countMuons = cms.bool(True),
+    countTaus = cms.bool(False),
+    minNumber = cms.uint32(2),
+    maxNumber = cms.uint32(999),
+)
+process.nanoAOD_step.insert(0, cms.Sequence(process.selectEl + process.selectMu + process.skimNLeps))
+"""))
+    if analysis == "frqcd":
+        for comp in selectedComponents:
+            comp.preprocessor = comp.preprocessor.clone(keepOutput = False, injectTriggerFilter = True, injectJSON = True)
+            if 'Mu' in comp.dataset:
+                comp.preprocessor = comp.preprocessor.clone(cfgHasFilter = True, inlineCustomize = """
+process.skim1Mu = cms.EDFilter("PATMuonRefSelector",
+    src = cms.InputTag("slimmedMuons"),
+    cut = cms.string("pt > %g && miniPFIsolation.chargedHadronIso < 0.45*pt && abs(dB('PV3D')) < 8*edB('PV3D')"),
+    filter = cms.bool(True),
+)
+process.nanoAOD_step.insert(0, process.skim1Mu)
+""" % (7.5 if "DoubleMuon" in comp.dataset else 4.5))
+            elif 'QCD_Pt' in comp.dataset or "EGamma" in comp.dataset or "SingleElectron" in comp.dataset or "DoubleEG" in comp.dataset:
+                comp.preprocessor = comp.preprocessor.clone(cfgHasFilter = True, inlineCustomize = """
+process.skim1El = cms.EDFilter("PATElectronRefSelector",
+    src = cms.InputTag("slimmedElectrons"),
+    cut = cms.string("pt > 6 && miniPFIsolation.chargedHadronIso < 0.45*pt && abs(dB('PV3D')) < 8*edB('PV3D')"),
+    filter = cms.bool(True),
+)
+process.nanoAOD_step.insert(0, process.skim1El)
+""")
+
+# print summary of components to process
+if getHeppyOption("justSummary"): 
+    print "**************************************************************"
+    print "*****************   SUMMARY     ******************************"
+    print "**************************************************************"
+    printSummary(selectedComponents)
+    sys.exit(0)
+
+from CMGTools.TTHAnalysis.tools.nanoAOD.addTnpTree import addTnpTree
+from PhysicsTools.NanoAODTools.postprocessing.framework.postprocessor import PostProcessor
+#from CMGTools.TTHAnalysis.tools.nanoAOD.WZ_modules import *
+# in the cut string, keep only the main cuts to have it simpler
+from PhysicsTools.NanoAODTools.postprocessing.modules.common.puWeightProducer import *
+#MC
+modules = [puAutoWeight_2017G(), addTnpTree(year,flavor)]
+cut = 'n%s > 1'%flavor
+
+compression = "ZLIB:3" #"LZ4:4" #"LZMA:9"
+branchsel_in = os.environ['CMSSW_BASE']+"/src/CMGTools/TTHAnalysis/python/tools/nanoAOD/branchsel_in.txt"
+branchsel_out = None
+
+POSTPROCESSOR = PostProcessor(None, [], modules = modules,
+        cut = cut, prefetch = True, longTermCache = False,
+        branchsel = branchsel_in, outputbranchsel = branchsel_out, compression = compression)
+
+test = getHeppyOption("test")
+if test == "94X-MC":
+    TTLep_pow = kreator.makeMCComponent("TTLep_pow", "/TTTo2L2Nu_mtop166p5_TuneCP5_PSweights_13TeV-powheg-pythia8/RunIIFall17MiniAOD-94X_mc2017_realistic_v10-v1/MINIAODSIM", "CMS", ".*root", 831.76*((3*0.108)**2) )
+    TTLep_pow.files = ["/afs/cern.ch/user/g/gpetrucc/cmg/NanoAOD_94X_TTLep.root"]
+    lepSkim.requireSameSignPair = False
+    lepSkim.minJets = 0
+    lepSkim.minMET = 0
+    lepSkim.prescaleFactor = 0
+    selectedComponents = [TTLep_pow]
+elif test == "94X-MC-miniAOD":
+    TTLep_pow = kreator.makeMCComponent("TTLep_pow", "/TTTo2L2Nu_mtop166p5_TuneCP5_PSweights_13TeV-powheg-pythia8/RunIIFall17MiniAOD-94X_mc2017_realistic_v10-v1/MINIAODSIM", "CMS", ".*root", 831.76*((3*0.108)**2) )
+    TTLep_pow.files = [ 'root://cms-xrd-global.cern.ch//store/mc/RunIIFall17MiniAOD/TTTo2L2Nu_mtop166p5_TuneCP5_PSweights_13TeV-powheg-pythia8/MINIAODSIM/94X_mc2017_realistic_v10-v1/70000/3CC234EB-44E0-E711-904F-FA163E0DF774.root' ]
+    localfile = os.path.expandvars("/tmp/$USER/%s" % os.path.basename(TTLep_pow.files[0]))
+    if os.path.exists(localfile): TTLep_pow.files = [ localfile ] 
+    from CMGTools.Production.nanoAODPreprocessor import nanoAODPreprocessor
+    TTLep_pow.preprocessor = nanoAODPreprocessor("/afs/cern.ch/work/g/gpetrucc/ttH/CMSSW_10_4_0/src/nanov4_NANO_cfg.py")
+    selectedComponents = [TTLep_pow]
+elif test == "102X-MC":
+    TTLep_pow = kreator.makeMCComponent("TTLep_pow", "/TTTo2L2Nu_TuneCP5_13TeV-powheg-pythia8/RunIIAutumn18NanoAODv4-Nano14Dec2018_102X_upgrade2018_realistic_v16-v1/NANOAODSIM", "CMS", ".*root", 831.76*((3*0.108)**2), useAAA=True )
+    #215AB578-F603-7D45-A3F6-8A9B2432BC74.root
+#RunIIAutumn18NanoAODv4/TTTo2L2Nu_TuneCP5_13TeV-powheg-pythia8/NANOAODSIM/Nano14Dec2018_102X_upgrade2018_realistic_v16-v1/70000/B94C9288-F6D3-CD48-9023-46C29A1AFD27.root
+    TTLep_pow.files = ['root://cms-xrd-global.cern.ch//store/mc/RunIIAutumn18NanoAODv4/TTTo2L2Nu_TuneCP5_13TeV-powheg-pythia8/NANOAODSIM/Nano14Dec2018_102X_upgrade2018_realistic_v16-v1/70000/B94C9288-F6D3-CD48-9023-46C29A1AFD27.root']
+    #TTLep_pow.files = TTLep_pow.files[:1]
+    selectedComponents = [TTLep_pow]
+
+elif test == "94X-data":
+    json = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions17/13TeV/ReReco/Cert_294927-306462_13TeV_EOY2017ReReco_Collisions17_JSON_v1.txt'
+    #json = 'Cert_294927-306462_13TeV_EOY2017ReReco_Collisions17_JSON_v1.txt'
+    SingleElectron_Run2017C_14Dec2018 = kreator.makeDataComponent("SingleElectron_Run2017C_14Dec2018", "/SingleElectron/Run2017C-Nano14Dec2018-v1/NANOAOD", "CMS", ".*root", json)
+   # SingleElectron_Run2017C_14Dec2018.files = ["56ACED37-6B39-E242-B776-A28C50904F3F.root"]
+    SingleElectron_Run2017C_14Dec2018.files = ['root://xrootd-cms.infn.it//store/data/Run2017C/SingleElectron/NANOAOD/Nano14Dec2018-v1/90000/56ACED37-6B39-E242-B776-A28C50904F3F.root']
+    SingleElectron_Run2017C_14Dec2018.triggers = triggerGroups_dict["Trigger_1e"][year]
+    SingleElectron_Run2017C_14Dec2018.vetoTriggers = triggerGroups_dict["Trigger_2m"][year] + triggerGroups_dict["Trigger_3m"][year]+triggerGroups_dict["Trigger_2e"][year] + triggerGroups_dict["Trigger_3e"][year]+triggerGroups_dict["Trigger_em"][year] + triggerGroups_dict["Trigger_mee"][year] + triggerGroups_dict["Trigger_mme"][year]+triggerGroups_dict["Trigger_1m"][year]
+    selectedComponents = [SingleElectron_Run2017C_14Dec2018]
+
+#nanopy.py testTnP test.py --option test="5TeV-data" --option flavor=Electron
+#5TeV test data    flavor="Electron" 
+elif test == "5TeV-data":
+    json = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions17/5TeV/ReReco/Cert_306546-306826_5TeV_EOY2017ReReco_Collisions17_JSON.txt'
+    HighEGJet = kreator.makeMyPrivateDataComponent("HighEGJet","/HighEGJet/jrgonzal-data5TeV_22nov2019_HighEGJet_Run2017G-17Nov2017-v2-e6071589c1d4feaedf45b2e5392eb06a/USER", "PRIVATE", ".*root", "phys03", json, None, [], [],True)
+    HighEGJet.files = ['root://xrootd-cms.infn.it//store/user/jrgonzal/NANOAOD/HighEGJet/data5TeV_22nov2019_HighEGJet_Run2017G-17Nov2017-v2/191122_134710/0001/myNanoProdMc5TeV_NANO_1555.root']
+    HighEGJet.triggers = triggerGroups_dict["Trigger_5TeV_1e"][year] + triggerGroups_dict["Trigger_5TeV_2e"][year]
+    selectedComponents = [HighEGJet]
+
+elif test in ('2','3','3s'):
+    doTestN(test, selectedComponents)
+
