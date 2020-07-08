9bdf1536b83f796d86b29fd51f52188e83805f89
Adeik <15350389+vrbouza@users.noreply.github.com>
2020-07-08 13:06:38 +0200

    Working script of obtaining cards producing commands. TODO: adding same executing functionality as with the plotterHelper.py.

--- TTHAnalysis/python/plotter/tw-run2/cardsHelper.py
+++ TTHAnalysis/python/plotter/tw-run2/cardsHelper.py
@@ -13,42 +13,39 @@ friendfolders = ["0_yeartag", "1_lepmerge_roch", "2_cleaning", "3_varstrigger", 
 lumidict      = {2016 : 35.92, 2017 : 41.53, 2018 : 59.74}
 
 
-friendsscaff = "--Fs {fpath}/{p}/{y}/0_yeartag --Fs {fpath}/{p}/{y}/1_lepmerge_roch --Fs {fpath}/{p}/{y}/2_cleaning --Fs {fpath}/{p}/{y}/3_varstrigger --FMCs {fpath}/{p}/{y}/4_scalefactors --Fs {fpath}/{p}/{y}/5_mvas"
+friendsscaff = "--Fs {P}/0_yeartag --Fs {P}/1_lepmerge_roch --Fs {P}/2_cleaning --Fs {P}/3_varstrigger --FMCs {P}/4_scalefactors --Fs {P}/5_mvas"
 
 slurmscaff   = "sbatch -c {nth} -p {queue} -J {jobname} -e {logpath}/log.%j.%x.err -o {logpath}/log.%j.%x.out --wrap '{command}'"
 
-commandscaff = '''python makeShapeCardsNew.py --tree NanoAOD {mcafile} {cutsfile} "{variable}" "{bins}" {samplespaths} {friends} --od {outpath} --s2v -l {lumi} {nthreads} -f -L tw-run2/functions_tw.cc --neg --threshold 0.01 -W "MuonSF * ElecSF * TrigSF * puWeight * bTagWeight{extraweights}" --year {year} {asimovornot} --unc tw-run2/uncs-tw.txt --amc'''
-
-#### SCAFFOLD
-ASIMOV="--asimov signal"
+commandscaff = '''python makeShapeCardsNew.py --tree NanoAOD {mcafile} {cutsfile} "{variable}" "{bins}" {samplespaths} {friends} --od {outpath} --s2v -l {lumi} {nth} -f -L tw-run2/functions_tw.cc --neg --threshold 0.01 -W "MuonSF * ElecSF * TrigSF * puWeight * bTagWeight * PrefireWeight" --year {year} {asimovornot} {uncs} {extra}'''
 
 
-def CardsCommand(prod, year, nthreads, outpath, selplot, region, extra):
-    mcafile_   = "tw-run2/mca-tw-{y}.txt".format(y = year)
-    cutsfile_  = "tw-run2/cuts-tw-{reg}.txt".format(reg = region)
+
+def CardsCommand(prod, year, var, bines, isAsimov, nthreads, outpath, region, noUnc, useFibre, extra):
+    mcafile_   = "tw-run2/mca-tw.txt"
+    cutsfile_  = "tw-run2/cuts-tw-{reg}.txt".format( reg = region)
     plotsfile_ = "tw-run2/plots-tw-{reg}.txt".format(reg = region)
 
-    samplespaths_ = "-P " + mcpath + "/" + str(year) + " -P " + datapath + "/" + str(year)
-    extraweights_ = "" if year == 2018 else " * PrefireWeight"
+    samplespaths_ = "-P " + friendspath + "/" + prod + ("/" + year) * (year != "run2")
+    if useFibre: samplespaths_ = samplespaths_.replace("phedexrw", "phedex").replace("cienciasrw", "ciencias")
 
     nth_       = "" if nthreads == 0 else ("--split-factor=-1 -j " + str(nthreads))
-    friends_   = friendsscaff.format(fpath = friendspath,
-                                     p     = prod,
-                                     y     = year)
-    outpath_   = outpath + "/" + str(year) + "/" + region
+    friends_   = friendsscaff
+    outpath_   = outpath + "/" + year + "/" + region
 
 
     comm = commandscaff.format(outpath      = outpath_,
                                friends      = friends_,
                                samplespaths = samplespaths_,
-                               lumi      = lumidict[year],
+                               lumi      = lumidict[int(year)],
+                               variable  = var,
+                               bins      = bines,
                                nth       = nth_,
                                year      = year,
-                               extraweights = extraweights_,
-                               selplot   = "" if selplot == "" else ("--sP " + selplot),
+                               asimovornot = "--asimov signal" if isAsimov else "",
                                mcafile   = mcafile_,
                                cutsfile  = cutsfile_,
-                               plotsfile = plotsfile_,
+                               uncs      = "--unc tw-run2/uncs-tw.txt --amc" if not noUnc else "--amc",
                                extra     = extra)
 
     return comm
@@ -56,14 +53,19 @@ def CardsCommand(prod, year, nthreads, outpath, selplot, region, extra):
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser(usage = "python nanoAOD_checker.py [options]", description = "Checker tool for the outputs of nanoAOD production (NOT postprocessing)", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('--production','-P', metavar = "prod",      dest = "prod",    required = True)
-    parser.add_argument('--year',      '-y', metavar = 'year',      dest = "year",    required = False, default = 2016, type = int)
-    parser.add_argument('--queue',     '-q', metavar = 'queue',     dest = "queue",   required = False, default = "")
-    parser.add_argument('--extraArgs', '-e', metavar = 'extra',     dest = "extra",   required = False, default = "")
-    parser.add_argument('--nthreads',  '-j', metavar = 'nthreads',  dest = "nthreads",required = False, default = 0, type = int)
-    parser.add_argument('--pretend',   '-p', action = "store_true", dest = "pretend", required = False, default = False)
-    parser.add_argument('--outpath',   '-o', metavar = 'outpath',   dest = "outpath", required = False, default = "./temp/varplots")
-    parser.add_argument('--region',    '-r', metavar = 'region',    dest = "region",  required = False, default = "1j1t")
+    parser.add_argument('--production','-P', metavar = "prod",       dest = "prod",     required = True)
+    parser.add_argument('--year',      '-y', metavar = 'year',       dest = "year",     required = False, default = "2016")
+    parser.add_argument('--queue',     '-q', metavar = 'queue',      dest = "queue",    required = False, default = "")
+    parser.add_argument('--extraArgs', '-e', metavar = 'extra',      dest = "extra",    required = False, default = "")
+    parser.add_argument('--nthreads',  '-j', metavar = 'nthreads',   dest = "nthreads", required = False, default = 0, type = int)
+    parser.add_argument('--pretend',   '-p', action  = "store_true", dest = "pretend",  required = False, default = False)
+    parser.add_argument('--outpath',   '-o', metavar = 'outpath',    dest = "outpath",  required = False, default = "./temp/cards")
+    parser.add_argument('--region',    '-r', metavar = 'region',     dest = "region",   required = False, default = "1j1t")
+    parser.add_argument('--nounc',     '-u', action  = "store_true", dest = "nounc",    required = False, default = False)
+    parser.add_argument('--variable',  '-v', metavar = 'variable',   dest = "variable", required = False, default = "getBDtW(SergioBDT_1j1b)")
+    parser.add_argument('--bines',     '-b', metavar = 'bines',      dest = "bines",    required = False, default = "[0.5,1.5,2.5,3.5,4.5,5.5, 6.5, 7.5, 8.5, 9.5, 10.5]")
+    parser.add_argument('--asimov',    '-a', action  = "store_true", dest = "asimov",   required = False, default = False)
+    parser.add_argument('--useFibre',  '-f', action  = "store_true", dest = "useFibre", required = False, default = False)
 
 
     args     = parser.parse_args()
@@ -75,30 +77,10 @@ if __name__=="__main__":
     pretend  = args.pretend
     outpath  = args.outpath
     region   = args.region
+    noUnc    = args.nounc
+    variable = args.variable
+    bines    = args.bines
+    asimov   = args.asimov
+    useFibre = args.useFibre
 
-
-    if queue != "":
-        print "> Plotting jobs will be sent to the cluster."
-        if abs(year - 2017) > 1:
-            print "   - All three years will be plotted."
-            cont = False
-            if   pretend:
-                cont = True
-            elif confirm("Three jobs will be sent to queue {q} with {j} requested threads to plot in each year {pls}. Do you want to continue?".format(q = queue, j = nthreads, pls = "all the plots" if selplot == "" else selplot)):
-                cont = True
-
-            if cont:
-                for y in [2016, 2017, 2018]:
-                    GeneralExecutioner( (prod, y, nthreads, outpath, selplot, region, queue, extra, pretend) )
-        else:
-            cont = False
-            if   pretend:
-                cont = True
-            elif confirm("One job will be sent to queue {q} with {j} requested threads to plot in year {y} {pls}. Do you want to continue?".format(q = queue, j = nthreads, y = year, pls = "all the plots" if selplot == "" else selplot)):
-                cont = True
-
-            if cont:
-                GeneralExecutioner( (prod, year, nthreads, outpath, selplot, region, queue, extra, pretend) )
-    else:
-        print "> Local execution chosen."
-        GeneralExecutioner( (prod, year, nthreads, outpath, selplot, region, queue, extra, pretend) )
+    print CardsCommand(prod, year, variable, bines, asimov, nthreads, outpath, region, noUnc, useFibre, extra)
