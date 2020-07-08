e802af071070e4b053ddc74201149e56369aff51
Adeik <15350389+vrbouza@users.noreply.github.com>
2020-07-08 13:07:08 +0200

    Updated BDT values for 2016 rebinning.

--- TTHAnalysis/python/plotter/tw-run2/functions_tw.cc
+++ TTHAnalysis/python/plotter/tw-run2/functions_tw.cc
@@ -31,17 +31,26 @@ int getnLooseJets(int nlj, int nj) {
 
 
 float getBDtW(double BDT) {
-  if      (BDT < -0.531448  ) return 1;
-  else if (BDT < -0.396646  ) return 2;
-  else if (BDT < -0.282771  ) return 3;
-  else if (BDT < -0.184872  ) return 4;
-  else if (BDT < -0.0977834 ) return 5;
-  else if (BDT < -0.0118005 ) return 6;
-  else if (BDT < 0.0867415  ) return 7;
-  else if (BDT < 0.207723   ) return 8;
-  else if (BDT < 0.332052   ) return 9;
-  else                        return 10;
+  if      (BDT < -0.425391) return 1;
+  else if (BDT < -0.317214) return 2;
+  else if (BDT < -0.228295) return 3;
+  else if (BDT < -0.149703) return 4;
+  else if (BDT < -0.075527) return 5;
+  else if (BDT < -0.008082) return 6;
+  else if (BDT < 0.070643)  return 7;
+  else if (BDT < 0.161760)  return 8;
+  else if (BDT < 0.272351)  return 9;
+  else                      return 10;
+}
 
+
+float getBDtWOther(double BDT) {
+  if      (BDT < -0.165024) return 1;
+  else if (BDT < -0.069230) return 2;
+  else if (BDT < -0.001835) return 3;
+  else if (BDT < 0.047763)  return 4;
+  else if (BDT < 0.111411)  return 5;
+  else                      return 6;
 }
 
 
