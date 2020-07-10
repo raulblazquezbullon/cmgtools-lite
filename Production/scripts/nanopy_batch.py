0abcac8232564cd0842578b8f4ce442ea7bb4262
cramonal <clara.ramon.alvarez@cern.ch>
2020-07-10 17:21:59 +0200

    fix to run data

--- Production/scripts/nanopy_batch.py
+++ Production/scripts/nanopy_batch.py
@@ -43,7 +43,8 @@ eval $(scram runtime -sh)
 popd
 echo
 mkdir cache
-export TMPDIR=/tmp/sscruz/`shuf -zer -n20  {A..Z} {a..z} {0..9}`/
+export TMPDIR=/tmp/$USER/`shuf -zer -n20  {A..Z} {a..z} {0..9}`/
+mkdir -p $TMPDIR
 mkdir job
 cd job
 echo '==== copying job dir to worker ===='
@@ -51,6 +52,8 @@ cp ../* .
 """
        dirCopy = """
 mv Loop/* ../.
+# cleanup
+rm -r $TMPDIR
 """
       
    else: # shared filesystem
