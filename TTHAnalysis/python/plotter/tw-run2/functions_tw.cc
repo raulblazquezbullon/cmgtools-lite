eebd74b247b631d22cf185494e26e43320af0308
Adeik <15350389+vrbouza@users.noreply.github.com>
2020-07-13 17:32:59 +0200

    Modified rebinning functions to support new trainings.

--- TTHAnalysis/python/plotter/tw-run2/functions_tw.cc
+++ TTHAnalysis/python/plotter/tw-run2/functions_tw.cc
@@ -30,6 +30,29 @@ int getnLooseJets(int nlj, int nj) {
 }
 
 
+float getSergioBDtW(double BDT) {
+  if      (BDT < -0.425391) return 1;
+  else if (BDT < -0.317214) return 2;
+  else if (BDT < -0.228295) return 3;
+  else if (BDT < -0.149703) return 4;
+  else if (BDT < -0.075527) return 5;
+  else if (BDT < -0.008082) return 6;
+  else if (BDT < 0.070643)  return 7;
+  else if (BDT < 0.161760)  return 8;
+  else if (BDT < 0.272351)  return 9;
+  else                      return 10;
+}
+
+
+float getSergioBDtWOther(double BDT) {
+  if      (BDT < -0.165024) return 1;
+  else if (BDT < -0.069230) return 2;
+  else if (BDT < -0.001835) return 3;
+  else if (BDT < 0.047763)  return 4;
+  else if (BDT < 0.111411)  return 5;
+  else                      return 6;
+}
+
 float getBDtW(double BDT) {
   if      (BDT < -0.425391) return 1;
   else if (BDT < -0.317214) return 2;
