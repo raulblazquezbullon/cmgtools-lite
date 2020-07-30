de7c55b258d4d884374c578719ec83f18d5ef4f9
Carlos <cvicovillalba1998@gmail.com>
2020-07-30 10:48:01 +0200

    Made some changes

--- TTHAnalysis/python/plotter/5TeV_LowPu/5TeV_LowPu_plots.py
+++ TTHAnalysis/python/plotter/5TeV_LowPu/5TeV_LowPu_plots.py
@@ -11,7 +11,7 @@ lumi = 0.30432
 
 submit = '{command}'
 
-
+nCores = 4
 P0 = "/nfs/fanae/user/cvico/Workspace/WZ_lowPU/FriendTrees/5TeV_Jul27"
 
 TREESALL = " --F {P}/lepGood_fTrees --FMCs {P}/1_recleaning_v1 --Fs {P}/1_recleaning_v1_data --FMCs {P}/2_eventVars_mc --Fs {P}/2_eventVars_data"
@@ -24,6 +24,7 @@ def base(selection):
     '''
     THETREES = TREESALL
     CORE = ' '.join([THETREES, TREESONLYSKIM]) 
+    CORE += " -f -j {nCores} -l {lumi} -L".format(nCores = nCores, lumi = lumi)
+    CORE += " -L 5TeV_LowPu/functions5TeV_lowPu.cc --tree NanoAOD --mcc 5TeV_LowPu/lepchoice-FO.txt"
     print(CORE)
-    
 base("bla")
