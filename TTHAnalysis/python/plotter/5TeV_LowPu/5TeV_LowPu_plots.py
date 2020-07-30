196ab0844c2c1bbeec24ddd1f6c5636715035214
Carlos <cvicovillalba1998@gmail.com>
2020-07-30 10:43:13 +0200

    Started implementing selection function

---
+++ TTHAnalysis/python/plotter/5TeV_LowPu/5TeV_LowPu_plots.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+import sys
+import re
+import os
+
+
+ODIR = sys.argv[1]
+dowhat = sys.argv[2]
+
+lumi = 0.30432
+
+submit = '{command}'
+
+
+P0 = "/nfs/fanae/user/cvico/Workspace/WZ_lowPU/FriendTrees/5TeV_Jul27"
+
+TREESALL = " --F {P}/lepGood_fTrees --FMCs {P}/1_recleaning_v1 --Fs {P}/1_recleaning_v1_data --FMCs {P}/2_eventVars_mc --Fs {P}/2_eventVars_data"
+TREESONLYSKIM = "-P "+P0
+
+def base(selection):
+    '''
+    This function is where the different
+    available selections are defined
+    '''
+    THETREES = TREESALL
+    CORE = ' '.join([THETREES, TREESONLYSKIM]) 
+    print(CORE)
+    
+base("bla")
