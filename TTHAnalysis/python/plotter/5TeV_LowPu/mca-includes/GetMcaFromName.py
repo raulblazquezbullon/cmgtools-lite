3dfb56d141deb6dd9f4e649d7f0e1bee7e7914cc
Carlos <cvicovillalba1998@gmail.com>
2020-07-30 12:01:50 +0200

    New script for getting mca-files

---
+++ TTHAnalysis/python/plotter/5TeV_LowPu/mca-includes/GetMcaFromName.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# This is a macro used to get mca-files
+# from just the name of the sample
+
+import os
+import re
+import sys
+
+path = sys.argv[1]
+#NameInFile = sys.argv[2]
+#mcaName = sys.argv[3]
+
+submit = "{command}"
+
+def GetListOfTrees(path):
+    # First we get a list of the files in the given path
+    files = os.listdir(path)
+    return files
+
+def GetKey(files, NameInFile):
+    # Return a list with all the files containing
+    # the key
+    samples = [f for f in files if NameInFile in f]
+    return samples
+
+files = GetListOfTrees(path)
+print(files)
+
