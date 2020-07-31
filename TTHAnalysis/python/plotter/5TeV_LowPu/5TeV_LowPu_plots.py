101cd36f79ae3bc02c7c254924ee49035d9db1ad
Carlos <cvicovillalba1998@gmail.com>
2020-07-31 17:53:21 +0200

    Implemented the basic parser options I will use

--- TTHAnalysis/python/plotter/5TeV_LowPu/5TeV_LowPu_plots.py
+++ TTHAnalysis/python/plotter/5TeV_LowPu/5TeV_LowPu_plots.py
@@ -8,6 +8,8 @@ parser = OptionParser(usage = "%prog [options]")
 
 # Options
 parser.add_option("-P", "--path", dest = "paths", type = "string", default = ".", help = "Specify the path in where to read the samples")
+parser.add_option("-F", "--friendPath", dest = "friendpaths", type = "string", default = ".", help = "Specify the paths where friends are stored")
+parser.add_option("-d", "--doWhat", dest = "doWhat", type = "string", default = "plot", help = "Specify what to do")
+parser.add_option("-l", "--lumi", dest = "lumi", type = int, default = 0.22, help = "Specify the luminosity in fb^-1")
+parser.add_option("-S", "--Selection", dest = "Selection", type = "string", help = "Specify selection")
 (options, args) = parser.parse_args()
-
-print (options.paths)
