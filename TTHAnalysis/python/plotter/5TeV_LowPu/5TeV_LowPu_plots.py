023b0164e7adc9fb50db24a21ae6ac3d3bddb995
Carlos <cvicovillalba1998@gmail.com>
2020-07-31 17:39:53 +0200

    First parser options implemented

--- TTHAnalysis/python/plotter/5TeV_LowPu/5TeV_LowPu_plots.py
+++ TTHAnalysis/python/plotter/5TeV_LowPu/5TeV_LowPu_plots.py
@@ -4,3 +4,10 @@ import re
 import os
 from optparse import OptionParser
 
+parser = OptionParser(usage = "%prog [options]")
+
+# Options
+parser.add_option("-P", "--path", dest = "paths", type = "string", default = ".", help = "Specify the path in where to read the samples")
+(options, args) = parser.parse_args()
+
+print (options.paths)
