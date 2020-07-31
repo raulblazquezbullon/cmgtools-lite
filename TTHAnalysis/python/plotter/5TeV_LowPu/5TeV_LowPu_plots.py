84f87c80ffb417aaca15dcff766da91022bae149
Carlos <cvicovillalba1998@gmail.com>
2020-07-31 17:29:06 +0200

    First commit on this new script

--- TTHAnalysis/python/plotter/5TeV_LowPu/5TeV_LowPu_plots.py
+++ TTHAnalysis/python/plotter/5TeV_LowPu/5TeV_LowPu_plots.py
@@ -4,14 +4,3 @@ import re
 import os
 from optparse import OptionParser
 
-lumi = 0.30432
-
-submit = '{command}'
-
-nCores = 4
-P0 = "/nfs/fanae/user/cvico/Workspace/WZ_lowPU/FriendTrees/5TeV_Jul27"
-
-TREESALL = " --F {P}/lepGood_fTrees --FMCs {P}/1_recleaning_v1 --Fs {P}/1_recleaning_v1_data --FMCs {P}/2_eventVars_mc --Fs {P}/2_eventVars_data"
-TREESONLYSKIM = "-P "+P0
-
-
