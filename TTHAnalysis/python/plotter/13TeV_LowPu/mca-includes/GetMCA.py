31c22678f170caf24e0da7f8a8fa956314919224
Carlos <cvicovillalba1998@gmail.com>
2020-08-01 12:39:33 +0200

    Have to redo the script since I almost messed it by accident :D

--- TTHAnalysis/python/plotter/13TeV_LowPu/mca-includes/GetMCA.py
+++ TTHAnalysis/python/plotter/13TeV_LowPu/mca-includes/GetMCA.py
@@ -13,7 +13,7 @@ P0 = "/pool/ciencias/nanoAODv6/lowPU2017/2020_07_21_postProc"
 parser = OptionParser(usage = "%prog [options]")
 
 parser.add_option("-P", "--path", dest = "path", type = "string", default = P0, help = "Path where to get the name of the samples")
-parser.add_option("-f", "--filename", dest = "name", type = "string", default = "", help = "Name of the sample to be added to the file. It works if you just give a short but key part of the name, but try to be as clear as possible")
+parser.add_option("-f", "--filename", dest = "filename", type = "string", default = "", help = "Name of the sample to be added to the file. It works if you just give a short but key part of the name, but try to be as clear as possible")
 parser.add_option("-k", "--key", dest = "key", type = "string", default = "key", help = "Name that will appear in the plots")
 parser.add_option("-n", "--norm", dest = "norm", type = float, default = 1.0, help = "Normalisation for the MC")
 parser.add_option("-C", "--color", dest = "color", type = "string", default = "ROOT.kRed", help = "Color for the histogram")
@@ -51,21 +51,24 @@ def WriteMCAfile(file):
 	process[s] = process[s].replace(process[s][-5:], "") #Remove the .root extension
         text += '{s}'.format(s = process[s]) 
 	if s != len(process)-1: text+=' + '# Files with the same name but exclusive to each other must go added in the mca-file
-    text += ' : {norm}'.format(norm = options.norm)
-    text += ' ; genSumWeightName="genEventSum_W"'
-    text += ',FillColor={color}'.format(color = options.color)
+
+    # mca files for data have different format
+    if options.key != "data":
+	    text += ' : {norm}'.format(norm = options.norm)
+	    text += ' ; genSumWeightName="genEventSum_W"'
+	    text += ',FillColor={color}'.format(color = options.color)
     file.write(text + "\n")	
     return 
 
 if __name__ == '__main__':
-	if options.key == "key": raise RuntimeError, 'No key was specified!'
+	if options.filename == "": raise RuntimeError, 'No filename was specified! Specify it with the -f {filename} option.'
 	# First thing we do is read the files from the path
 	pathCopy = options.path #Just to be secure about the path that is being read
 	files = GetListOfTrees(pathCopy)
 	# Let's get a list with only the files containing the key
-	process = GetKey(files, options.key)
-	if len(process)==0: raise RuntimeError, 'No file in {p} matched with the key: {k}'.format(p = pathCopy, k = options.key)	
-	if options._debug: print("[DEBUG]List of files to be added to the mca-file: \n {list}".format(list = process))
+	process = GetKey(files, options.filename)
+	if len(process)==0: raise RuntimeError, 'No file in {p} matched with the key: {f}'.format(p = pathCopy, f = options.filename)	
+	if options._debug: print("[DEBUG] List of files to be added to the mca-file: \n {list}".format(list = process))
 	
 	# Now that we have the keys, let's write the mca-file!
 	if options.outfile == "foo.txt": warnings.warn("You did not specify the output filename, so I'm saving it as foo.txt...")
