ba8fa0d781fa38406a4659a004b1883f2a719f8c
Carlos <cvicovillalba1998@gmail.com>
2020-08-04 22:08:55 +0200

    Started handling ftrees stuff

--- TTHAnalysis/python/plotter/13TeV_LowPu/plotterComand.py
+++ TTHAnalysis/python/plotter/13TeV_LowPu/plotterComand.py
@@ -4,7 +4,7 @@ import re
 import os 
 import warnings
 from optparse import OptionParser
-
+import argparse
 
 # This is a macro made for getting the command used to do
 # the plotting
@@ -15,17 +15,46 @@ ftreesPath = "~/Workspace/WZ_LowPu/FriendTrees/13TeV_lowPu/"
 parser = OptionParser(usage = "%prog [options]")
 
 parser.add_option("-P", "--path", dest = "path", type = "string", default = path, help = "Path where the postproc is storaged")
-parser.add_option("--fTree", dest = "ftreesPath", type = "string", default = ftreesPath, help = "Path where the friend-trees are storaged" )
+parser.add_option("-F", "--ftreesPath", dest = "ftreesPath", type = "string", default = ftreesPath, help = "Path where the friend-trees are storaged" )
 parser.add_option("-c", "--ch", dest = "channel", type = "string", default = "ttbar", help = "Channel in which the analisys is based")
-
+parser.add_option("--ftrees", dest = "ftrees", type = "string", default = "", action = "append", help = "Specify the ftrees that will be used")
 
 (options, args) = parser.parse_args()
 
+
+def AskUser(msg):
+    ans = ""
+    while ans not in ["y", "n", "yes", "no"]:
+        ans = raw_input(msg + " [Y/N]\n").lower()
+    return ans[0] == "y"
+
+
+def ProcessListOfFTrees(listOfTrees):
+    
+    print(listOfTrees)
+    
+def GetFtreesStuff():
+    # First, get the path
+    if options.ftreesPath == ftreesPath: print("No samples path option was given. Searching in {p} by default...".format(p = options.ftreesPath))
+    
+    # Check if there's any specified friend-tree that is to be runned
+    useAll = False
+    if options.ftrees == "": 
+        msg = "No fTrees have been specified. Do you want to use all of the fTrees default in the directory: {p}? [Y/N]".format(p = options.ftreesPath)
+        if AskUser(msg): useAll = True
+        
+    listOfTrees = os.listdir(options.ftreesPath) if useAll else options.ftrees
+    
+    return 
+
 if __name__ == '__main__':
     
+    # Let's start building the command'
+    command = 'python mcPlots.py'
+    
+    # Get the trees 
     TREES = options.path
-    FTREES = options.ftreesPath
-    if TREES == options.path: print("No samples path option was given. Searching in {p} by default...".format(p = options.path))
-    if FTREES == options.ftreesPath: print("No samples path option was given. Searching in {p} by default...".format(p = options.ftreesPath))
-    command = 'python mcPlots.py -P {path}'.format(path = options.path)
+    if TREES == path: print("No samples path option was given. Searching in {p} by default...".format(p = options.path))
+    command += ' -P {path}'.format(path = options.path)
+    GetFtreesStuff()
     print(command)
