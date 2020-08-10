409c50bfe0c2e9478fa431c4e79d69b115570fbc
Carlos <cvicovillalba1998@gmail.com>
2020-08-10 11:28:13 +0200

    Change class with settings for dictionaries

--- TTHAnalysis/python/tools/nanoAOD/LowPu_modules.py
+++ TTHAnalysis/python/tools/nanoAOD/LowPu_modules.py
@@ -4,17 +4,34 @@ import enum
 
 # Low Pile-Up modules used for friend-trees
 
-class setts(enum.IntEnum):
-    '''
-     Configuration settings for lepton selections
-    '''
-    muPt = 5,
-    elePt = 7,
-    miniRelIso = 0.4,
-    sip3d = 8,
-    dxy = 0.05,
-    dz = 0.1
-    
+#class setts(enum.IntEnum):
+    #'''
+     #Configuration settings for lepton selections
+    #'''
+    #muPt = 5,
+    #elePt = 7,
+    #miniRelIso = 0.4,
+    #sip3d = 8,
+    #dxy = 0.05,
+    #dz = 0.1
+
+# =================================== Parameter configuration
+
+muons = {
+    "pt"            : 5,
+    "miniRelIso"    : 0.4,
+    "sip3d"         : 8,
+    "dxy"           : 0.05,
+    "dz"            :0.1
+    }
+electrons = {
+    "pt"            : 7,
+    "miniRelIso"    : 0.4,
+    "sip3d"         : 8,
+    "dxy"           : 0.05,
+    "dz"            :0.1
+    }
+
 class ch(enum.IntEnum):
     '''
      Considered channels
@@ -37,12 +54,11 @@ class tags(enum.IntEnum):
     LowEGJet   = 6
 
 # ========================== Lepton Selections
-muonSelection = lambda l: abs(l.eta) < 2.4 and l.pt > conf.muPt and l.miniPFRelIso_all < conf.miniRelIso \
-    and l.sip3d < conf.sip3d and abs(l.dxy) < conf.dxy and abs(l.dz) < conf.dz
+muonSelection = lambda l: abs(l.eta) < 2.4 and l.pt > muons["pt"] and l.miniPFRelIso_all < muons["miniRelIso"] \
+    and l.sip3d < muons["sip3d"] and abs(l.dxy) < muons["dxy"] and abs(l.dz) < muons["dz"]
 
-electronSelection = lambda l: abs(l.eta) < 2.5 and l.pt > conf.elePt and l.miniPFRelIso_all < conf.miniRelIso \
-    and l.sip3d < conf.sip3d and abs(l.dxy) < conf.dxy and abs(l.dz) < conf.dz
-
+electronSelection = lambda l: abs(l.eta) < 2.4 and l.pt > electrons["pt"] and l.miniPFRelIso_all < electrons["miniRelIso"] \
+    and l.sip3d < electrons["sip3d"] and abs(l.dxy) < electrons["dxy"] and abs(l.dz) < electrons["dz"]
 
 # ===============================
 # Basic lepton modules
