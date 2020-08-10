def59088a46971d38e393f2afac02a41c85c18c6
Carlos <cvicovillalba1998@gmail.com>
2020-08-10 11:21:16 +0200

    Implemented lepton selection settings

--- TTHAnalysis/python/tools/nanoAOD/LowPu_modules.py
+++ TTHAnalysis/python/tools/nanoAOD/LowPu_modules.py
@@ -4,8 +4,17 @@ import enum
 
 # Low Pile-Up modules used for friend-trees
 
-
-
+class setts(enum.IntEnum):
+    '''
+     Configuration settings for lepton selections
+    '''
+    muPt = 5,
+    elePt = 7,
+    miniRelIso = 0.4,
+    sip3d = 8,
+    dxy = 0.05,
+    dz = 0.1
+    
 class ch(enum.IntEnum):
     '''
      Considered channels
@@ -26,3 +35,16 @@ class tags(enum.IntEnum):
     doubleMuon = 4
     HighEGJet  = 5
     LowEGJet   = 6
+
+# ========================== Lepton Selections
+muonSelection = lambda l: abs(l.eta) < 2.4 and l.pt > conf.muPt and l.miniPFRelIso_all < conf.miniRelIso \
+    and l.sip3d < conf.sip3d and abs(l.dxy) < conf.dxy and abs(l.dz) < conf.dz
+
+electronSelection = lambda l: abs(l.eta) < 2.5 and l.pt > conf.elePt and l.miniPFRelIso_all < conf.miniRelIso \
+    and l.sip3d < conf.sip3d and abs(l.dxy) < conf.dxy and abs(l.dz) < conf.dz
+
+
+# ===============================
+# LepMerging
+
+from PhysicsTools.NanoAODTools.postprocessing.modules.common.collectionMerger import collectionMerger
