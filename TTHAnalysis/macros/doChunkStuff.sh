6b6471c76d84c72e6da6370fc7b828c07012b34e
Carlos <cvicovillalba1998@gmail.com>
2020-08-12 12:29:34 +0200

    comments to clarify myself

--- TTHAnalysis/macros/doChunkStuff.sh
+++ TTHAnalysis/macros/doChunkStuff.sh
@@ -4,14 +4,14 @@ filesPath="$1"
 theoreticalCommand="sh chunkDealer.sh ~/Workspace/WZ_LowPu/FriendTrees/13TeV_Aug6/1_recleaning/ merge"
 
 cd ${filesPath}
+# There's always a chunk0 file for each sample, so we get a list of those
 files=$(ls | grep ".*chunk1.root")
 
 for f in $files
 do
+    # Use the chunk keyword as a delimitr and get the fileName
     var=$(echo ${f}|awk -F'.chunk' '{print $1}')
     echo $var
 done
-
-#var=$(echo ${f}|sed "s|$filesPath||g"|sed "s/$chunkStuff//g")
 
 cd -
