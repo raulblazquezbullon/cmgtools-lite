b450382aef5d5c5deabed6bae4637566ca7cf160
Carlos <cvicovillalba1998@gmail.com>
2020-08-12 16:34:05 +0200

    Trying to make a chunks directory and leaving in the main file only the merged friend-trees

--- TTHAnalysis/macros/doChunkStuff.sh
+++ TTHAnalysis/macros/doChunkStuff.sh
@@ -10,8 +10,11 @@ files=$(ls | grep ".*chunk1.root")
 for f in $files
 do
     # Use the chunk keyword as a delimitr and get the fileName
-    var=$(echo ${f}|awk -F'.chunk' '{print $1}')
-    echo $var
+    var=$(echo ${f}|awk -F'Friend.chunk' '{print $1}')
+    echo "I'm currently reading this sample: ${var}"
+    CMD="/home/cvico/Prueba/scriptDePrueba.sh ${filesPath} ${var} "
+    sh ${CMD}
+    break
 done
 
 cd -
