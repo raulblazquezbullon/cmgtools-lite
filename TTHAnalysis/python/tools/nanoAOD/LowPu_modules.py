9e5f52f0320e9e5076bc4474b04564bd5c208d1e
Carlos <cvicovillalba1998@gmail.com>
2020-09-06 10:19:28 +0200

    Made a new trigger dictionary with the triggers that are being/were used in the WZ analysis

--- TTHAnalysis/python/tools/nanoAOD/LowPu_modules.py
+++ TTHAnalysis/python/tools/nanoAOD/LowPu_modules.py
@@ -191,15 +191,40 @@ WZ_lowPu_recl_data = [recleaner_step1, recleaner_step2_data]
 
 # ===========================================================
 # ======================== Step 3 modules: Triggers
+
+# OLD DICT: Before contacting the WZ analysis authors
+#triggerGroups = dict(    
+#    Trigger_1e = {
+#        2017 : lambda ev : bool(getattr(ev, 'HLT_Ele35_WPTight_Gsf'))
+#               if (ev.datatag == tags.MC) else
+#               bool(getattr(ev, 'HLT_HIEle20_WPLoose_Gsf')) 
+#	       or bool(getattr(ev, 'HLT_HIEle40_WPLoose_Gsf'))
+#        },
+#    Trigger_1m = {
+#        2017 : lambda ev : bool(getattr(ev, 'HLT_IsoMu27'))
+#               if (ev.datatag == tags.MC) else 
+#               bool(getattr(ev, 'HLT_HIMu17'))
+#        },
+#    Trigger_2e = {
+#        2017 : lambda ev : bool(getattr(ev, 'HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_DZ')) 
+#	       or bool(getattr(ev, 'HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL')) 
+#               if (ev.datatag == tags.MC) else 
+#               bool(getattr(ev, 'HLT_HIEle20_Ele12_CaloIdL_TrackIdL_IsoVL_DZ')) 
+#               or bool(getattr(ev, 'HLT_HIEle20_Ele12_CaloIdL_TrackIdL_IsoVL'))
+#        }
+#    
+#    )
+
+# NEW DICT: 
 triggerGroups = dict(    
     Trigger_1e = {
-        2017 : lambda ev : bool(getattr(ev, 'HLT_Ele35_WPTight_Gsf'))
+        2017 : lambda ev : bool(getattr(ev, 'HLT_Ele20_WPLoose_Gsf'))
                if (ev.datatag == tags.MC) else
                bool(getattr(ev, 'HLT_HIEle20_WPLoose_Gsf')) 
 	       or bool(getattr(ev, 'HLT_HIEle40_WPLoose_Gsf'))
         },
     Trigger_1m = {
-        2017 : lambda ev : bool(getattr(ev, 'HLT_IsoMu27'))
+        2017 : lambda ev : bool(getattr(ev, 'HLT_Mu17'))
                if (ev.datatag == tags.MC) else 
                bool(getattr(ev, 'HLT_HIMu17'))
         },
