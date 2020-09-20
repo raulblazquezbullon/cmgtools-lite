4c7ef1e0e66699a28031684f57542ce70bc5f77f
Adeik <15350389+vrbouza@users.noreply.github.com>
2020-09-20 13:03:19 +0200

    IMPORTANT: these two files were modified to try to include more variations than JEC's (to be precise: lepton energy variations). As it could not be done, everything was migrated to Python (to the pythonCleaningTopRun2.py script). Please, take into account that these files are not functional as they are. Some code should be commented, other uncommented, and then they could be restore. If needed, compare with the files that were before this commit.

--- TTHAnalysis/python/tools/nanoAOD/cleaningTopRun2.py
+++ TTHAnalysis/python/tools/nanoAOD/cleaningTopRun2.py
@@ -8,9 +8,10 @@ import warnings as wr
 import os, sys
 
 class cleaningTopRun2(Module):
-    def __init__(self, label, jetPts, jetPtNoisyFwd, jetCollection = 'Jet',
+    def __init__(self, label, jetPts, jetPtNoisyFwd, jetCollection = 'Jet', lepCollection = "LepGood",
                  jetBTag = 'btagDeepFlavB', btagWP_ = "M", isMC = None,
-                 variations = ["jesTotalCorr", "jesTotalUnCorr", "jer"],
+                 jecvars = ["jesTotalCorr", "jesTotalUnCorr", "jer"],
+                 lepenvars = ["mu"],
                  year_ = None, debug = False):
 
         self.label = "" if (label in ["", None]) else ("_" + label)
@@ -22,14 +23,16 @@ class cleaningTopRun2(Module):
         self.selecsdict[2018] = lambda jet: jet.jetId > 1
 
         self.jc     = jetCollection
+        self.lc     = lepCollection
 
         self.jetPts = jetPts
         self.jetPtNoisyFwd = jetPtNoisyFwd
         self.jetBTag   = jetBTag
         if isMC is not None:
             self.isMC = isMC
-        self.variations = variations
-        self.year       = year_
+        self.jecvars   = jecvars
+        self.lepenvars = lepenvars
+        self.year      = year_
 
         #### NOTE: the b-tagging algorithm is hardcoded to be deep flavour, although this can be relatively
         # easyly modified to support other algorithms.
@@ -42,61 +45,139 @@ class cleaningTopRun2(Module):
     #### Running methods
     # New code
     def beginJob(self, histFile = None, histDirName = None):
-        self.vars = ["eta", "phi", "mass"]
-        self.vars_leptons = ["pdgId", 'jetIdx', 'pt', "mass"]
-
-        self.systsJEC = {0 : ""}
+        #### Configuring variations (lepton energy corrs. and JEC).
+        self.systsLepEn = {}
+        self.systsJEC   = {0 : ""}
         if self.isMC:
-            for sys in range(len(self.variations)):
-                self.systsJEC[sys+1]    = '_' + self.variations[sys] + 'Up'
-                self.systsJEC[-(sys+1)] = '_' + self.variations[sys] + 'Down'
+            for sys in range(len(self.jecvars)):
+                self.systsJEC[sys+1]    = '_' + self.jecvars[sys] + 'Up'
+                self.systsJEC[-(sys+1)] = '_' + self.jecvars[sys] + 'Down'
+            for sys in range(len(self.lepenvars)):
+                self.systsLepEn[sys+1]    = '_' + self.lepenvars[sys] + 'Up'
+                self.systsLepEn[-(sys+1)] = '_' + self.lepenvars[sys] + 'Down'
 
+
+
+        #### Configuring output branches
         self.outmasses   = ['minMllAFAS']
 
         self._outjetvars    = [x%self.jc for x in ['nB%sSelMedium%%d','n%sSel%%d']]
         self._outfwdjetvars = [x%self.jc for x in ['nFwd%sSel%%d']]
         self.outjetvars  = []
         for jetPt in self.jetPts:
-            self.outjetvars.extend([(x%jetPt+y, 'I' if ('nB%s'%self.jc in x or 'n%s'%self.jc in x) else 'F') for x in self._outjetvars    for y in self.systsJEC.values() if y != "" or "nB" in x])
-            self.outjetvars.extend([(x%jetPt+y, 'I' if ('nB%s'%self.jc in x or 'n%s'%self.jc in x) else 'F') for x in self._outfwdjetvars for y in self.systsJEC.values() if y != "" or "nB" in x])
+            self.outjetvars.extend([(x%jetPt+y, 'I') for x in self._outjetvars    for y in self.systsJEC.values()   if "nB" in x])
+            self.outjetvars.extend([(x%jetPt+y, 'I') for x in self._outfwdjetvars for y in self.systsJEC.values()   if "nB" in x])
 
+            self.outjetvars.extend([(x%jetPt+y, 'I') for x in self._outjetvars    for y in self.systsLepEn.values() if "nB" in x])
+            self.outjetvars.extend([(x%jetPt+y, 'I') for x in self._outfwdjetvars for y in self.systsLepEn.values() if "nB" in x])
+
+            #self.outjetvars.extend([(x%jetPt+y, 'I' if ('nB%s'%self.jc in x or 'n%s'%self.jc in x) else 'F') for x in self._outjetvars    for y in self.systsJEC.values() if y != "" or "nB" in x])
+            #self.outjetvars.extend([(x%jetPt+y, 'I' if ('nB%s'%self.jc in x or 'n%s'%self.jc in x) else 'F') for x in self._outfwdjetvars for y in self.systsJEC.values() if y != "" or "nB" in x])
+
+            #self.outjetvars.extend([(x%jetPt+y, 'I' if ('nB%s'%self.jc in x or 'n%s'%self.jc in x) else 'F') for x in self._outjetvars    for y in self.systsLepEn.values() if y != "" or "nB" in x])
+            #self.outjetvars.extend([(x%jetPt+y, 'I' if ('nB%s'%self.jc in x or 'n%s'%self.jc in x) else 'F') for x in self._outfwdjetvars for y in self.systsLepEn.values() if y != "" or "nB" in x])
         self.branches = [var + self.label for var in self.outmasses]
+        self.branches.extend([var + realvar + self.label for var in self.outmasses for delta,realvar in self.systsLepEn.iteritems()])
         self.branches.extend([(var + self.label, _type) for var,_type in self.outjetvars])
 
 
-        #### Configuring C++ helpers
-        self.vars_jets = [("pt", "pt_nom") if self.isMC else 'pt', 'btagDeepFlavB'] + [ 'pt_%s%s'%(x,y) for x in self.variations for y in ["Up", "Down"]]
-        self.vars_fwdjets = [("pt", "pt_nom") if self.isMC else 'pt'] + [ 'pt_%s%s'%(x,y) for x in self.variations for y in ["Up", "Down"]]
+        #### Configuring input branches
+        self.vars = ["eta", "phi"]
+        self.vars_leptons = ["pdgId", 'jetIdx', "mass"]
+        self.vars_jets = ([("pt", "pt_nom") if self.isMC else 'pt', ("mass", "mass_nom") if self.isMC else "mass", 'btagDeepFlavB'] +
+                          ['pt_%s%s'%(x,y) for x in self.jecvars for y in ["Up", "Down"] ] + ['mass_%s%s'%(x,y) for x in self.jecvars for y in ["Up", "Down"]])
+        self.vars_fwdjets = ([("pt", "pt_nom") if self.isMC else 'pt'] +
+                             [ 'pt_%s%s'%(x,y) for x in self.jecvars for y in ["Up", "Down"]] + ['mass_%s%s'%(x,y) for x in self.jecvars for y in ["Up", "Down"]])
         self.vars_jets_int = (["hadronFlavour"] if self.isMC else [])
         self.vars_jets_nooutput = []
 
-        self._helper_jetsSup    = CollectionSkimmer("{col}Sel{pt}".format(   col = self.jc, pt = self.jetPts[0]) + self.label, self.jc, floats = [], saveSelectedIndices = True, maxSize = 5)
-        self._helper_jetsInf    = CollectionSkimmer("{col}Sel{pt}".format(   col = self.jc, pt = self.jetPts[1]) + self.label, self.jc, floats = [], saveSelectedIndices = True, maxSize = 5)
 
-        self._helper_fwdjetsSup = CollectionSkimmer("Fwd{col}Sel{pt}".format(col = self.jc, pt = self.jetPts[0]) + self.label, self.jc, floats = [], saveSelectedIndices = True, maxSize = 5)
-        self._helper_fwdjetsInf = CollectionSkimmer("Fwd{col}Sel{pt}".format(col = self.jc, pt = self.jetPts[1]) + self.label, self.jc, floats = [], saveSelectedIndices = True, maxSize = 5)
+        #### Configuring C++ helpers for JEC variations as well as nominal results
+        self._helpers      = []
+        self._helpers_jecs = []
+        for delta,sys in self.systsJEC.iteritems():
+            setattr(self, "_helper_jetsSup" + sys,
+                    CollectionSkimmer("{col}Sel{pt}".format(   col = self.jc, pt = self.jetPts[0]) + sys + self.label, self.jc, floats = [], saveSelectedIndices = True, maxSize = 5))
+            setattr(self, "_helper_jetsInf" + sys,
+                    CollectionSkimmer("{col}Sel{pt}".format(   col = self.jc, pt = self.jetPts[1]) + sys + self.label, self.jc, floats = [], saveSelectedIndices = True, maxSize = 5))
 
+            setattr(self, "_helper_fwdjetsSup" + sys,
+                    CollectionSkimmer("Fwd{col}Sel{pt}".format(col = self.jc, pt = self.jetPts[0]) + sys + self.label, self.jc, floats = [], saveSelectedIndices = True, maxSize = 5))
+            setattr(self, "_helper_fwdjetsInf" + sys,
+                    CollectionSkimmer("Fwd{col}Sel{pt}".format(col = self.jc, pt = self.jetPts[1]) + sys + self.label, self.jc, floats = [], saveSelectedIndices = True, maxSize = 5))
 
-        self._helpers = [self._helper_jetsSup, self._helper_jetsInf, self._helper_fwdjetsSup, self._helper_fwdjetsInf]
+            if sys == "":
+                self._helpers.append(getattr(self, "_helper_jetsSup"    + sys))
+                self._helpers.append(getattr(self, "_helper_jetsInf"    + sys))
+                self._helpers.append(getattr(self, "_helper_fwdjetsSup" + sys))
+                self._helpers.append(getattr(self, "_helper_fwdjetsInf" + sys))
+            else:
+                self._helpers_jecs.append(getattr(self, "_helper_jetsSup"    + sys))
+                self._helpers_jecs.append(getattr(self, "_helper_jetsInf"    + sys))
+                self._helpers_jecs.append(getattr(self, "_helper_fwdjetsSup" + sys))
+                self._helpers_jecs.append(getattr(self, "_helper_fwdjetsInf" + sys))
+
+        #### Compiling C++ library in case it hasn't been done yet
         if "/fullCleanerTop.so" not in r.gSystem.GetLibraries():
             print "\n> C++ library not loaded. Compiling..."
             r.gROOT.ProcessLine(".L {base}/src/CMGTools/TTHAnalysis/python/tools/fullCleanerTop.cxx+O".format(base = os.environ['CMSSW_BASE']))
             print "> Library compiled!"
 
+        # Create main object and set its properties
         self._worker = r.fullCleanerTop(self._helper_jetsSup.cppImpl(), self._helper_jetsInf.cppImpl(), self._helper_fwdjetsSup.cppImpl(), self._helper_fwdjetsInf.cppImpl(), -99)
-
         self._worker.setJetPts(self.jetPts[0], self.jetPts[1])
         self._worker.setFwdNoisyPt(self.jetPtNoisyFwd)
+
+        # Create main object and set its properties
+        for delta,sys in self.systsJEC.iteritems():
+            if sys == "": continue
+            print delta, sys
+            self._worker.setJECVariation(delta,
+                                         getattr(self, "_helper_jetsSup"    + sys).cppImpl(),
+                                         getattr(self, "_helper_jetsInf"    + sys).cppImpl(),
+                                         getattr(self, "_helper_fwdjetsSup" + sys).cppImpl(),
+                                         getattr(self, "_helper_fwdjetsInf" + sys).cppImpl())
+
+        # Configuring C++ helpers for lepton energy variations
+        self._helpers_lepen = []
+        for delta,sys in self.systsLepEn.iteritems():
+            setattr(self, "_helper_jetsSup" + sys,
+                    CollectionSkimmer("{col}Sel{pt}".format(   col = self.jc, pt = self.jetPts[0]) + sys + self.label, self.jc, floats = [], saveSelectedIndices = True, maxSize = 5))
+            setattr(self, "_helper_jetsInf" + sys,
+                    CollectionSkimmer("{col}Sel{pt}".format(   col = self.jc, pt = self.jetPts[1]) + sys + self.label, self.jc, floats = [], saveSelectedIndices = True, maxSize = 5))
+
+            setattr(self, "_helper_fwdjetsSup" + sys,
+                    CollectionSkimmer("Fwd{col}Sel{pt}".format(col = self.jc, pt = self.jetPts[0]) + sys + self.label, self.jc, floats = [], saveSelectedIndices = True, maxSize = 5))
+            setattr(self, "_helper_fwdjetsInf" + sys,
+                    CollectionSkimmer("Fwd{col}Sel{pt}".format(col = self.jc, pt = self.jetPts[1]) + sys + self.label, self.jc, floats = [], saveSelectedIndices = True, maxSize = 5))
+
+            self._helpers_lepen.append(getattr(self, "_helper_jetsSup"    + sys))
+            self._helpers_lepen.append(getattr(self, "_helper_jetsInf"    + sys))
+            self._helpers_lepen.append(getattr(self, "_helper_fwdjetsSup" + sys))
+            self._helpers_lepen.append(getattr(self, "_helper_fwdjetsInf" + sys))
+
+            print getattr(self, "_helper_jetsSup"    + sys).cppImpl(), getattr(self, "_helper_jetsInf"    + sys).cppImpl(), getattr(self, "_helper_fwdjetsSup" + sys).cppImpl(), getattr(self, "_helper_fwdjetsInf" + sys).cppImpl(),
+
+            setattr(self, "_worker" + sys, r.fullCleanerTop(getattr(self, "_helper_jetsSup"    + sys).cppImpl(),
+                                                            getattr(self, "_helper_jetsInf"    + sys).cppImpl(),
+                                                            getattr(self, "_helper_fwdjetsSup" + sys).cppImpl(),
+                                                            getattr(self, "_helper_fwdjetsInf" + sys).cppImpl(),
+                                                            -99))
+            getattr(self, "_worker" + sys).setJetPts(self.jetPts[0], self.jetPts[1])
+            getattr(self, "_worker" + sys).setFwdNoisyPt(self.jetPtNoisyFwd)
+        print dir(self)
         return
 
 
     def beginFile(self, inputFile, outputFile, inputTree, wrappedOutputTree):
-        for x in self._helpers:
+        for x in (self._helpers + self._helpers_lepen + self._helpers_jecs):
             x.initInputTree(inputTree)
+
         self.initReaders(inputTree)
         self.initWorkers()
+
         declareOutput(self, wrappedOutputTree, self.branches)
-        for x in self._helpers:
+        for x in (self._helpers + self._helpers_lepen + self._helpers_jecs):
             x.initOutputTree(wrappedOutputTree.tree(), True);
         return
 
@@ -122,38 +203,65 @@ class cleaningTopRun2(Module):
             if selection(jet): tags.setJetFlags(i, True)
 
         if self._ttreereaderversion != event._tree._ttreereaderversion:
-            for x in self._helpers: x.initInputTree(event._tree)
+            for x in (self._helpers + self._helpers_lepen): x.initInputTree(event._tree)
             self.initReaders(event._tree)
             self.initWorkers()
 
-        for x in self._helpers: x.clear()
+        for x in (self._helpers + self._helpers_lepen): x.clear()
 
         self._worker.clear()
         self._worker.loadTags(tags, wpM)
         self._worker.run()
 
         for delta,varname in self.systsJEC.iteritems():
-            for x in self._worker.GetJetSums(delta):
+            thefunction = "GetNominalJetSums"
+            if delta != 0: thefunction = "GetJetSums"
+            for x in getattr(self._worker, thefunction)(delta):
                 for var in self._outjetvars:
-                    if varname != "" or "nB" in var: self.wrappedOutputTree.fillBranch(var%x.thr + varname + self.label, getattr(x, var.replace('%d', '').replace(self.jc, 'Jet')))
+                    if "nB" in var: self.wrappedOutputTree.fillBranch(var%x.thr + varname + self.label, getattr(x, var.replace('%d', '').replace(self.jc, 'Jet')))
                 for var in self._outfwdjetvars:
-                    if varname != "" or "nB" in var: self.wrappedOutputTree.fillBranch(var%x.thr + varname + self.label, getattr(x, var.replace('%d', '').replace(self.jc, 'Jet')))
+                    if "nB" in var: self.wrappedOutputTree.fillBranch(var%x.thr + varname + self.label, getattr(x, var.replace('%d', '').replace(self.jc, 'Jet')))
+                #for var in self._outjetvars:
+                    #if varname != "" or "nB" in var: self.wrappedOutputTree.fillBranch(var%x.thr + varname + self.label, getattr(x, var.replace('%d', '').replace(self.jc, 'Jet')))
+                #for var in self._outfwdjetvars:
+                    #if varname != "" or "nB" in var: self.wrappedOutputTree.fillBranch(var%x.thr + varname + self.label, getattr(x, var.replace('%d', '').replace(self.jc, 'Jet')))
 
         minMllAFAS = self._worker.GetPairMasses()
-        for var in self.outmasses:
-            self.wrappedOutputTree.fillBranch(var + self.label, minMllAFAS)
+        for m in self.outmasses:
+            self.wrappedOutputTree.fillBranch(m + self.label, minMllAFAS)
+
+        for delta,var in self.systsLepEn.iteritems():
+            getattr(self, "_worker" + var).clear()
+            getattr(self, "_worker" + var).loadTags(tags, wpM)
+            getattr(self, "_worker" + var).run()
+
+            for x in self._worker.GetJetSums(0):
+                for jetvar in self._outjetvars:
+                    if "nB" in var: self.wrappedOutputTree.fillBranch(jetvar%x.thr + var + self.label, getattr(x, jetvar.replace('%d', '').replace(self.jc, 'Jet')))
+                for jetvar in self._outfwdjetvars:
+                    if "nB" in var: self.wrappedOutputTree.fillBranch(jetvar%x.thr + var + self.label, getattr(x, jetvar.replace('%d', '').replace(self.jc, 'Jet')))
+
+            tmpmass = getattr(self, "_worker" + var).GetPairMasses()
+            for m in self.outmasses:
+                self.wrappedOutputTree.fillBranch(m + var + self.label, tmpmass)
 
         return True
 
 
     # General methods
-    def initReaders(self,tree):
+    def initReaders(self, tree):
         self._ttreereaderversion = tree._ttreereaderversion
-        for coll in ["LepGood", self.jc]:
+        for coll in [self.lc, self.jc] + [self.lc + var[1:] for delta,var in self.systsLepEn.iteritems()]:
             setattr(self, 'n' + coll, tree.valueReader('n' + coll))
             _vars = self.vars[:]
-            if coll == 'LepGood': _vars.extend(self.vars_leptons)
-            if coll == self.jc:   _vars.extend(self.vars_jets + self.vars_jets_int + self.vars_jets_nooutput)
+            if   coll == self.lc:
+                _vars.extend(self.vars_leptons)
+                _vars.append(("pt", 'pt_corrAll'))
+            elif self.lc in coll: ### lepton energy variated collections
+                _vars.extend(self.vars_leptons)
+                _vars.append(("pt", 'pt_' + coll.replace(self.lc, "")))
+            elif coll == self.jc:
+                _vars.extend(self.vars_jets + self.vars_jets_int + self.vars_jets_nooutput)
 
             for B in _vars:
                 if type(B) == tuple:
@@ -173,12 +281,29 @@ class cleaningTopRun2(Module):
         jecs = r.vector("TTreeReaderArray<float>*")()
 
         if self.isMC:
-            for var in self.variations:
-                jecs.push_back( getattr(self, '%s_pt_%sUp'%(self.jc,   var)))
-                jecs.push_back( getattr(self, '%s_pt_%sDown'%(self.jc, var)))
+            for var in self.jecvars:
+                jecs.push_back( getattr(self, '%s_pt_%sUp'%(self.jc,     var)))
+                jecs.push_back( getattr(self, '%s_pt_%sDown'%(self.jc,   var)))
 
         self._worker.setJets(getattr(self,'n%s'%self.jc), getattr(self,'%s_pt'%self.jc), getattr(self,'%s_eta'%self.jc), getattr(self,'%s_phi'%self.jc),
                              getattr(self,'%s_%s'%(self.jc,self.jetBTag)),
                              jecs
                             )
+
+        for delta,var in self.systsLepEn.iteritems():
+            collName = self.lc + var[1:]
+            getattr(self, "_worker" + var).setLeptons(getattr(self, "n" + collName),
+                                                      getattr(self, collName + "_pt"),
+                                                      getattr(self, collName + "_eta"),
+                                                      getattr(self, collName + "_phi"),
+                                                      getattr(self, collName + "_mass"),
+                                                      getattr(self, collName + "_jetIdx"))
+            getattr(self, "_worker" + var).setJets(getattr(self,'n%s'%self.jc),
+                                                   getattr(self,'%s_pt'%self.jc),
+                                                   getattr(self,'%s_eta'%self.jc),
+                                                   getattr(self,'%s_phi'%self.jc),
+                                                   getattr(self,'%s_%s'%(self.jc, self.jetBTag)),
+                                                   r.vector("TTreeReaderArray<float>*")()
+                                                  )
+
         return
