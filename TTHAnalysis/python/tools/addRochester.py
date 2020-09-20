105f7db1a788622684c5bd12a412c73437abedea
Adeik <15350389+vrbouza@users.noreply.github.com>
2020-09-20 12:56:48 +0200

    Modified addRochester to comply with current nanoAOD files and also preparing to take into account rochester incs.

--- TTHAnalysis/python/tools/addRochester.py
+++ TTHAnalysis/python/tools/addRochester.py
@@ -4,11 +4,16 @@ from CMGTools.TTHAnalysis.treeReAnalyzer import Collection as CMGCollection
 from CMGTools.TTHAnalysis.tools.nanoAOD.friendVariableProducerTools import declareOutput, writeOutput
 
 class addRochester(Module):
-    def __init__(self, label = "LepGood_pt_corrAll"):
-        self.label    = label
-        self.branches = [(self.label,        "F", "", "nLepGood"),
-                         (self.label + "Up", "F", "", "nLepGood"),
-                         (self.label + "Dn", "F", "", "nLepGood")]
+    def __init__(self, label = "LepGood_pt_corrAll", labelvars = "LepGood_pt_mu", isMC = True):
+        self.label     = label
+        self.labelvars = labelvars
+        self.isMC      = isMC
+        self.branches  = [(self.label,              "F", "", "nLepGood"),
+                          (self.labelvars + "Up",   "F", "", "nLepGood"),
+                          (self.labelvars + "Down", "F", "", "nLepGood"),]
+        if self.isMC:
+          self.branches.append(("LepGoodmuUp_pt_muUp",   "F", "", "nLepGoodmuUp"))
+          self.branches.append(("LepGoodmuDown_pt_muDown", "F", "", "nLepGoodmuDown"))
         return
 
 
@@ -35,9 +40,11 @@ class addRochester(Module):
     def run(self, event, Collection):
         leps  = [l for l in Collection(event, "LepGood")]
 
-        newvals   = []
-        newvalsup = []
-        newvalsdn = []
+        newvals    = []
+        newvalsup  = []
+        newvalsup2 = []
+        newvalsdn  = []
+        newvalsdn2 = []
         for lep in leps:
             if abs(lep.pdgId) == 11:
                 newvals.append(  lep.pt)
@@ -48,9 +55,26 @@ class addRochester(Module):
                 newvalsup.append(lep.correctedUp_pt)
                 newvalsdn.append(lep.correctedDown_pt)
 
+        allret = {self.label           : newvals,
+                  self.labelvars + "Up": newvalsup,
+                  self.labelvars + "Down": newvalsdn,
+        }
 
+        if self.isMC:
+            leps  = [l for l in Collection(event, "LepGoodmuUp")]
+            for lep in leps:
+                if abs(lep.pdgId) == 11:
+                    newvalsup2.append(lep.pt)
+                else:
+                    newvalsup2.append(lep.correctedDown_pt)
+            leps  = [l for l in Collection(event, "LepGoodmuDown")]
+            for lep in leps:
+                if abs(lep.pdgId) == 11:
+                    newvalsdn2.append(lep.pt)
+                else:
+                    newvalsdn2.append(lep.correctedDown_pt)
 
-        allret = {self.label       : newvals,
-                  self.label + "Up": newvalsup,
-                  self.label + "Dn": newvalsdn}
+            allret["LepGoodmuUp_pt_muUp"]     = newvalsup2
+            allret["LepGoodmuDown_pt_muDown"] = newvalsdn2
+
         return allret
