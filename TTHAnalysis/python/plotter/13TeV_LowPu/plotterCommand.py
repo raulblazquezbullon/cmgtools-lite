e37f86bdb9df4de9552f91fd0e5c32ca6083f5f1
Carlos <cvicovillalba1998@gmail.com>
2020-10-01 17:08:19 +0200

    New way of dealing with that many lines

--- TTHAnalysis/python/plotter/13TeV_LowPu/plotterCommand.py
+++ TTHAnalysis/python/plotter/13TeV_LowPu/plotterCommand.py
@@ -8,7 +8,7 @@ import argparse
 import warnings as wr
         
 # =========== Default parameters
-CMD = "python mcPlots.py --tree NanoAOD {mca} {mcc} {plots}" # Base part of the command that we are gonna use in this Analysis
+CMD = "python mcPlots.py --tree NanoAOD {mca} {cuts} {plots}" # Base part of the command that we are gonna use in this Analysis
 
 
 defaultPars = {"path"       : "/pool/phedex/userstorage/cmstudents/cvico/WZ_LowPu/MiniTrees/14-07-2020/",        # path where the merged samples are stored
@@ -17,7 +17,7 @@ defaultPars = {"path"       : "/pool/phedex/userstorage/cmstudents/cvico/WZ_LowP
                "functions"  : "13TeV_LowPu/functions13TeV_lowPu.cc",															  # Some extra functions defined for the Analysis
               }
 
-
+steps = ["0_tags", "1_lepMerge", "2_recleaning", "3_triggers", "4_eventVars"]
 
 def add_options(parser):
 	
@@ -26,35 +26,8 @@ def add_options(parser):
 	parser.add_argument("--selection", 	'-S', metavar = "selection", 	dest = "selection",  default = "ttbar")
 	parser.add_argument("--outpath", 	'-o', metavar = "outpath", 	dest = "outpath",  		default = "/tmp/plots") # save plots in temporary folder (testing purposes)
 	return parser
-def add_format_stuff(text):
-    for step in steps: text += "--Fs {ftreesPath}{Friend} ".format(ftreesPath = defaultPars["ftreesPath"], Friend = step)
-    Lines = ["-L {functions} ".format(functions = defaultPars["functions"]),
-             "--split-factor=-1 ",
-             "--maxRatioRange 0.6  1.99 ", 
-             "--ratioYNDiv 505 ",
-             "--showRatio ",
-             "--attachRatioPanel ", 
-             "--fixRatioRange ",
-             "--legendColumns 3 ",
-             "--legendWidth 0.35 ",
-	     		 #"--mcc {mcc} ".format(mcc = defaultPars["mccFile"]),
-	     		 #"{mca} ".format(mca = defaultPars["mcaFile"]),
-             #"{cuts} ".format(cuts = defaultPars["cuts"]),
-             #"{plots} ".format(plots = defaultPars["plots"]),
-             #"{weight} ".format(weight = defaultPars["weight"]), #still have no weights to add
-             #"{binname} ".format(binname = "--binname ttbar"),
-             "--legendFontSize 0.042 "
-             "--noCms ",
-	     		 #"--rebin 4 ",
-             "--topSpamSize 1.1 ",
-             "--lspam '#scale[1.1]{#bf{CMS}} #scale[0.9]{#it{Preliminary}}' ",
-             "--rspam '%(lumi) (13 TeV)' ",
-             "--mcc {mccFile} ".format(mccFile = defaultPars["mccFile"]),
-             ]
-    
-    for line in Lines: text += line
-    
-    return text
+
+
 
 if __name__ == "__main__":
 	parser = add_options(argparse.ArgumentParser())
@@ -65,6 +38,26 @@ if __name__ == "__main__":
 	year 		  = options.year
 	selection  = options.selection
 	outpath    = options.outpath
+	
+	
+	Lines = ["--Fs {ftreesPath}{Friend} ".format(ftreesPath = defaultPars["ftreesPath"], Friend = step) for step in steps]
+	Lines.extend(["--split-factor=-1 ",
+             		  "--maxRatioRange 0.6  1.99 ", 
+             		  "--ratioYNDiv 505 ",
+						  "--showRatio ",
+						  "--attachRatioPanel ", 
+						  "--fixRatioRange ",
+						  "--legendColumns 3 ",
+						  "--legendWidth 0.35 ",
+						  "--legendFontSize 0.042 "
+						  "--noCms ",
+						  "--topSpamSize 1.1 ",
+						  "--lspam '#scale[1.1]{#bf{CMS}} #scale[0.9]{#it{Preliminary}}' ",
+						 "--rspam '%(lumi) (13 TeV)' "])
+	
+
+	print(Lines)	
+
 
 	
 
