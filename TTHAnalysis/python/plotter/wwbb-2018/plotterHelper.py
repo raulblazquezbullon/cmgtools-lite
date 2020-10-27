8768b35ab417ce906b6032ffd4f94941d05bb6a1
sheylaag <sheyla.alvarez@hotmail.com>
2020-10-27 13:47:21 +0100

    Changed from gen level to particle level plots file

--- TTHAnalysis/python/plotter/wwbb-2018/plotterHelper.py
+++ TTHAnalysis/python/plotter/wwbb-2018/plotterHelper.py
@@ -32,7 +32,7 @@ friendsscaff = "--Fs {P}/0_yeartag --Fs {P}/1_lepmerge_roch --Fs {P}/2_cleaning 
 #commandscaff = "python mcPlots.py --tree NanoAOD --pdir {outpath} {friends} {samplespaths} -f -l {lumi} {nth} --year {year} {ratio} --ratioYNDiv 210 --showRatio --attachRatioPanel --fixRatioRange --ratioNum twttbar --ratioDen wwbb --legendColumns 1 --legendWidth 0.32 --legendFontSize 0.042 --plotmode nostack --noCms --topSpamSize 1.1 --lspam '#scale[1.1]{{#bf{{CMS}}}} #scale[0.9]{{#it{{Preliminary}}}}' --showMCError -L wwbb-2018/functions_tw.cc {selplot} {mcafile} {cutsfile} {plotsfile} {extra}"
 
 # Post-separar SF
-commandscaff = "python mcPlots.py --tree NanoAOD --pdir {outpath} {friends} {samplespaths} -f -l {lumi} {nth} --year {year} {ratio} --ratioYNDiv 210 --showRatio --attachRatioPanel --fixRatioRange --legendColumns 3 --legendWidth 0.52 --legendFontSize 0.042 --noCms --topSpamSize 1.1 --lspam '#scale[1.1]{{#bf{{CMS}}}} #scale[0.9]{{#it{{Preliminary}}}}' --showMCError -W 'MuonIDSF * MuonISOSF * ElecIDSF * ElecRECOSF * TrigSF * puWeight * bTagWeight * PrefireWeight' -L wwbb-2018/functions_tw.cc {selplot} {mcafile} {cutsfile} {plotsfile} {extra}"
+commandscaff = "python mcPlots.py --tree NanoAOD --pdir {outpath} {friends} {samplespaths} -f -l {lumi} {nth} --year {year} {ratio} --ratioNum vvttv+nonworz+dy --ratioDen wwbb --ratioYNDiv 210 --showRatio --attachRatioPanel --fixRatioRange --legendColumns 3 --legendWidth 0.52 --legendFontSize 0.042 --noCms --topSpamSize 1.1 --lspam '#scale[1.1]{{#bf{{CMS}}}} #scale[0.9]{{#it{{Preliminary}}}}' --showMCError -W 'MuonIDSF * MuonISOSF * ElecIDSF * ElecRECOSF * TrigSF * puWeight * bTagWeight * PrefireWeight' -L wwbb-2018/functions_tw.cc {selplot} {mcafile} {cutsfile} {plotsfile} {extra}"
 
 
 
@@ -108,7 +108,7 @@ def GeneralExecutioner(task):
 def PlottingCommand(prod, year, nthreads, outpath, selplot, region, ratio, extra, useFibre, doUncs):
     mcafile_   = "wwbb-2018/mca-tw-wwbb.txt"
     cutsfile_  = "wwbb-2018/cuts-tw-wwbb-{reg}.txt".format(reg = region if "_" not in region else region.split("_")[0])
-    plotsfile_ = "wwbb-2018/plots-tw/plots-wwbb-gen-{reg}.txt".format(reg = region)
+    plotsfile_ = "wwbb-2018/plots-tw/plots-wwbb-{reg}.txt".format(reg = region)
 
     samplespaths_ = "-P " + friendspath + "/" + prod + ("/" + year) * (year != "run2")
     if useFibre: samplespaths_ = samplespaths_.replace("phedexrw", "phedex").replace("cienciasrw", "ciencias")
