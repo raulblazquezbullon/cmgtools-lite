4fdad8e0246d807044069b273b7bd21f579ab6cd
Adeik <15350389+vrbouza@users.noreply.github.com>
2020-12-03 18:28:56 +0100

    Fixed bug in the script for producing the uncertainty table.,

--- TTHAnalysis/python/plotter/tw-run2/produceUncertaintyTable.py
+++ TTHAnalysis/python/plotter/tw-run2/produceUncertaintyTable.py
@@ -1,3 +1,5 @@
+import os, sys
+
 systsGroup = {
     #### Statistical
     'mc_stat' : [
@@ -93,8 +95,10 @@ systsGroup = {
     'matching' : [
         "ttbar_matching",
     ],
-    #'scales' : [
-    #],
+    'scales' : [
+        "ttbar_scales",
+        "tw_scales",
+    ],
     'ps' : [
         "fsr_ttbar",
         "isr_ttbar",
@@ -115,11 +119,14 @@ systsGroup = {
 }
 
 dowhat = 'step1'
+#dowhat = 'step2'
 POIs   = ["r"]
 
-thecard = "./temp_2020_10_30/cards/combinada.root"
+pretend = False
 
-groupList   = ['mc_stat', 'jecs', 'trigger', 'pileup', 'lep', 'btag', 'lumi', 'prefiring', 'norm', "matching", "ps", "colour", "ue"]
+thecard = "./temp_2020_11_25/cards/combinada.root"
+
+groupList   = ['mc_stat', 'jecs', 'trigger', 'pileup', 'lep', 'btag', 'lumi', 'prefiring', 'norm', "matching", "scales", "ps", "colour", "ue"]
 
 basecommand = '\ncombineTool.py -M MultiDimFit --algo grid --points 100 --rMin 0 --rMax 3 --floatOtherPOIs=1 -m 125  --split-points 1 --setParameters r=1 -t -1 --expectSignal=1 --job-mode SGE --saveInactivePOI 1 '
 
@@ -127,23 +134,39 @@ if dowhat == 'step1':
     for poi in POIs:
         cumulative      = [x for x in ["r"] if x != poi]
 
-        print basecommand + '-n nominal_%s %s --task-name nominal_%s -P %s %s'%(poi, thecard, poi, poi, ",".join(cumulative))
+        nomcomm = basecommand + '-n nominal_%s %s --task-name nominal_%s -P %s %s'%(poi, thecard, poi, poi, ",".join(cumulative))
+        print "Command:", nomcomm
+        if not pretend: os.system(nomcomm)
 
-        print basecommand.replace('--algo grid','--algo none').replace("--points 100","").replace("--job-mode SGE","")+ '-n bestfit_%s --saveWorkspace tw-run2_WS.root -P %s '%(poi,poi)
+        gridcomm = basecommand.replace('--algo grid','--algo none').replace("--points 100","").replace("--job-mode SGE","")+ '-n bestfit_%s --saveWorkspace %s -P %s '%(poi, thecard, poi)
+        print "Command:", gridcomm
+        if not pretend: os.system(gridcomm)
 
         #print basecommand.replace('--algo grid','--algo none').replace("--points 300","").replace("--job-mode SGE","")+ '-n bestfit_%s --saveWorkspace combinada.root -P %s '%(poi,poi) ### de Soto
         for group in groupList:
             cumulative += systsGroup[group]
-            print basecommand + ' -P %s '%poi + '-n ' + group + '_%s'%poi + ' higgsCombinebestfit_%s.MultiDimFit.mH125.root --snapshotName MultiDimFit  --freezeParameters %s'%(poi,",".join(cumulative)) + ' --task-name %s_%s'%(group,poi)
+            thecomm = basecommand + ' -P %s '%poi + '-n ' + group + '_%s'%poi + ' higgsCombinebestfit_%s.MultiDimFit.mH125.root --snapshotName MultiDimFit  --freezeParameters %s'%(poi,",".join(cumulative)) + ' --task-name %s_%s'%(group,poi)
+            print "Command:", thecomm
+            if not pretend: os.system(thecomm)
 
 if dowhat == 'step2':
     for poi in POIs:
         fileList        = [ ]
-        print 'hadd higgsCombinenominal_%s.MultiDimFit.mH125.root higgsCombinenominal_%s.POINTS.*.MultiDimFit.mH125.root'%(poi,poi)
-        for gr in groupList:
-            print 'hadd higgsCombine%s.MultiDimFit.mH125.root higgsCombine%s.POINTS.*.MultiDimFit.mH125.root'%(gr+'_'+poi,gr+'_'+poi)
-            fileList.append( "'higgsCombine%s.MultiDimFit.mH125.root:Freeze += %s:%d'"%(gr+'_'+poi,gr,groupList.index(gr)))
+        nomcomm = 'hadd higgsCombinenominal_%s.MultiDimFit.mH125.root higgsCombinenominal_%s.POINTS.*.MultiDimFit.mH125.root'%(poi, poi)
+        print "Command:", nomcomm
+        if not pretend: os.system(nomcomm)
 
-        print 'plot1DScan.py higgsCombinenominal_%s.MultiDimFit.mH125.root --others '%poi +' '.join( fileList ) +' --breakdown '  + ','.join(groupList) +',stat'  + " --POI %s "%poi
-        print 'mv scan.pdf scan_%s.pdf'%poi
-        print 'mv scan.png scan_%s.png'%poi
+        for gr in groupList:
+            tmpcomm = 'hadd higgsCombine%s.MultiDimFit.mH125.root higgsCombine%s.POINTS.*.MultiDimFit.mH125.root'%(gr+'_'+poi,gr+'_'+poi)
+            fileList.append( "'higgsCombine%s.MultiDimFit.mH125.root:Freeze += %s:%d'"%(gr+'_'+poi,gr,groupList.index(gr)))
+            print "Command:", tmpcomm
+            if not pretend: os.system(tmpcomm)
+
+        thecomm = 'plot1DScan.py higgsCombinenominal_%s.MultiDimFit.mH125.root --others '%poi +' '.join( fileList ) +' --breakdown '  + ','.join(groupList) +',stat'  + " --POI %s "%poi
+        print "Command:", thecomm
+        if not pretend: os.system(thecomm)
+
+        thecomm = 'mv scan.pdf scan_%s.pdf; mv scan.png scan_%s.png'%(poi, poi)
+        print 'Command:', thecomm
+        if not pretend: os.system(thecomm)
+
