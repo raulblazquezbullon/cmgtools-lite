18da38bc2c83fe8aa50f7390cf420d51e9dbdc2d
Adeik <15350389+vrbouza@users.noreply.github.com>
2021-03-15 16:08:59 +0100

    Added threshold value to avoid empty bins.

--- TTHAnalysis/python/plotter/tw-run2/differential/getBinningForTheFits_moreprofiled.py
+++ TTHAnalysis/python/plotter/tw-run2/differential/getBinningForTheFits_moreprofiled.py
@@ -19,7 +19,8 @@ friendsscaff  = "--Fs {P}/0_yeartag --Fs {P}/1_lepmerge_roch --Fs {P}/2_cleaning
 slurmscaff    = "sbatch -c {nth} -p {queue} -J {jobname} -e {logpath}/log.%j.%x.err -o {logpath}/log.%j.%x.out --wrap '{command}'"
 
 #commandscaff  = '''python makeShapeCardsNew.py --tree NanoAOD {mcafile} {cutsfile} "{variable}" "{bins}" {samplespaths} {friends} --od {outpath} -l {lumi} {nth} -f -L {func} --neg -W "{w}" --year {year} {asimovornot} {uncs} {extra} {name} --AP --storeAll --notMinimumFill --notVarsChanges'''
-commandscaff  = '''python makeShapeCardsNew.py --tree NanoAOD {mcafile} {cutsfile} "{variable}" "{bins}" {samplespaths} {friends} --od {outpath} -l {lumi} {nth} -f -L {func} --neg -W "{w}" --year {year} {asimovornot} {uncs} {extra} {name} --AP --storeAll --notVarsChanges'''
+#commandscaff  = '''python makeShapeCardsNew.py --tree NanoAOD {mcafile} {cutsfile} "{variable}" "{bins}" {samplespaths} {friends} --od {outpath} -l {lumi} {nth} -f -L {func} --neg -W "{w}" --year {year} {asimovornot} {uncs} {extra} {name} --AP --storeAll --notVarsChanges'''
+commandscaff  = '''python makeShapeCardsNew.py --tree NanoAOD {mcafile} {cutsfile} "{variable}" "{bins}" {samplespaths} {friends} --od {outpath} -l {lumi} {nth} -f -L {func} --neg -W "{w}" --year {year} {asimovornot} {uncs} {extra} {name} --AP --storeAll --notVarsChanges --threshold 0.0001'''
 
 theweights    = "MuonIDSF * MuonISOSF * ElecIDSF * ElecRECOSF * TrigSF * puWeight * bTagWeight * PrefireWeight"
 minchunkbytes = 1000
@@ -1004,6 +1005,7 @@ if __name__=="__main__":
                     if not "scales" in iS and not "colour" in iS:
                         tasks.append( (inpath, iY, iV, samenuis, iS + "Down") )
         #print tasks
+        #for el in tasks: print el
         #sys.exit()
         print "> Executing..."
         if nthreads > 1:
