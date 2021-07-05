19f5cce4bb12cb4f9d5403252a48f0476d1d1c95
sheylaag <sheyla.alvarez@hotmail.com>
2021-07-05 11:53:30 +0200

    Updated friendscaff

--- TTHAnalysis/python/plotter/wwbb-2018/cardsHelper.py
+++ TTHAnalysis/python/plotter/wwbb-2018/cardsHelper.py
@@ -14,7 +14,7 @@ lumidict      = {2016 : 35.92, 2017 : 41.53, 2018 : 59.74}
 
 
 #friendsscaff = "--Fs {P}/0_yeartag --Fs {P}/1_lepmerge_roch --Fs {P}/2_cleaning --Fs {P}/3_varstrigger --FMCs {P}/4_scalefactors --Fs {P}/5_mvas"
-friendsscaff = "--Fs {P}/1_lepmerge_roch --Fs {P}/2_cleaning --Fs {P}/3_varstrigger --FMCs {P}/4_scalefactors"
+friendsscaff = "--FDs {P}/0_lumijson --Fs {P}/1_lepmerge_roch --Fs {P}/2_cleaning --Fs {P}/3_varstrigger --FMCs {P}/4_scalefactors --Fs {P}/5_mvas --Fs {P}/6_hemissue"
 
 slurmscaff   = "sbatch -c {nth} -p {queue} -J {jobname} -e {logpath}/log.%j.%x.err -o {logpath}/log.%j.%x.out --wrap '{command}'"
 
