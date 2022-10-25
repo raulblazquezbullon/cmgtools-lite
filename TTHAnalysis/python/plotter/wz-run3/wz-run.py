eb894a7e5b0b9d42cd8cdf5367dc5b2e701a22d1
Miguel Obeso <miguelom99@gmail.com>
2022-10-25 17:19:22 +0200

    change default production name

--- TTHAnalysis/python/plotter/wz-run3/wz-run.py
+++ TTHAnalysis/python/plotter/wz-run3/wz-run.py
@@ -22,7 +22,7 @@ def add_parsing_opts():
                 help = "Output (folder) name")
   parser.add_option("--isData", dest = "isData", action = "store_true", default = False, 
                 help = "Name of the last folder in which the output will be saved")
-  parser.add_option("--prodname", dest = "prodname", type="string", default = "Oct18", 
+  parser.add_option("--prodname", dest = "prodname", type="string", default = "oct22", 
                 help = "Name of the last folder in which the output will be saved")
   parser.add_option("--tier", dest = "tier", type=str, default = "nanoAODv10", 
                 help = "Production tier used in output path formats")
