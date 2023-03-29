8e98bdc8dbb661713fa3cdfb3d5768019a61b0f2
raulblazquezbullon <raulblazquezbullon@gmail.com>
2023-03-29 19:25:36 +0200

    Changed extension of wz-ws in run_impacts_inclusive.sh to .root. Added different options to autoCardCombiner.py in order to execute fits commands returned from run_impacts_inclusive.sh and executed autoCardCombiner.py

--- TTHAnalysis/python/plotter/autoCardCombiner.py
+++ TTHAnalysis/python/plotter/autoCardCombiner.py
@@ -23,6 +23,11 @@ def add_parsing_options():
 	parser = argparse.ArgumentParser()
 	parser.add_argument("--nquant",    dest = "nquant")
 	parser.add_argument("--variables", dest = "variables", default = functional_variables.keys())
+	parser.add_argument("--fit1",      dest = "fit1",action = "store_true",default = False)
+	parser.add_argument("--fit2",      dest = "fit2",action = "store_true",default = False)
+	parser.add_argument("--produce",   dest = "produce",action = "store_true",default = False)
+	parser.add_argument("--plot",      dest = "plot",action = "store_true",default = False)
+	parser.add_argument("--run_all",   dest = "run_all",action = "store_true",default = False)
 
 	return parser.parse_args() 
 	
@@ -31,6 +36,11 @@ if __name__ == "__main__":
 	opts = add_parsing_options()
 	nquant = opts.nquant.split(",")
 	variables = opts.variables.split(",")
+	fit1 = opts.fit1
+	fit2 = opts.fit2
+	produce = opts.produce
+	plot = opts.plot
+	run_all = opts.run_all
 	
 	main_path = "/nfs/fanae/user/rblazquez/TFG/wz_run3/CMSSW_12_4_12/src/CMGTools/TTHAnalysis/python/plotter/"
 	
@@ -38,13 +48,28 @@ if __name__ == "__main__":
 	filename = "run_impacts_inclusive.sh"
 	
 	for var in variables:
-		print(" >> Going to %s variable" %var)
+		print(">>> Going to %s variable" %var)
 		
 		for nq in nquant:
-			inpath = main_path + "./check_discriminant_vars/"
-			outpath = main_path + "./check_discriminant_vars/"
+			inpath = main_path + "./check_discriminant_vars/./rebin%s/%s/cards/" %(nq,var)
+			outpath = main_path + "./check_discriminant_vars/./rebin%s/%s/combined_cards/" %(nq,var)
 			
-			inpath += "./rebin%s/%s/cards/" %(nq,var)
-			outpath += "./rebin%s/%s/combined_cards/" %(nq,var)
-			
-			comm = subprocess.Popen(["sh",filename,inpath,outpath],cwd = filepath)
+			comm = subprocess.Popen(["sh",filename,inpath,outpath],cwd = filepath) # Invokes shell and runs run_impacts_inclusive.sh
+			cmd = (subprocess.check_output(["sh",filename,inpath,outpath],cwd = filepath).decode("utf-8")).split("\n")
+						
+			for index,line in enumerate(cmd):
+				if line == "---> Command to perform the initial fit" and (fit1 == True or run_all == True):
+					print("==== Performing initial fit ====")
+					out1 = subprocess.Popen(cmd[index + 2],shell = True)
+					
+				elif line == "---> Command to perform the sequential fits. PLEASE CHECK THE ENVIRONMENT IN WHICH THIS COMMAND WILL RUN (local or cluster)" and (fit2 == True or run_all == True):
+					print("==== Performing sequential fits ====")
+					out2 = subprocess.Popen(cmd[index + 2],shell = True)
+					
+				elif line == "---> Produce the impacts" and (produce == True or run_all == True):
+					print("==== Producing impacts ====")
+					out3 = subprocess.Popen(cmd[index + 2],shell = True)
+					
+				elif line == "---> Plot the impacts" and (produce == True or run_all == True):
+					print("==== Plotting impacts ====")
+					out4 = subprocess.Popen(cmd[index + 2],shell = True)
--- TTHAnalysis/python/plotter/wz-run3/scripts/combine/run_impacts_inclusive.sh
+++ TTHAnalysis/python/plotter/wz-run3/scripts/combine/run_impacts_inclusive.sh
@@ -8,17 +8,17 @@ echo Combining cards
 echo ===================
 if [[ ! -d $outpath ]]; then mkdir $outpath; fi
 # Get the script to combine the cards
-python fancyCombineCards.py --inpath $inpath --outpath $outpath 
+python fancyCombineCards.py --inpath $inpath --outpath $outpath #--pretend
 
 
 echo ===================
 echo Creating workspace
 echo ===================
 # Now generate the workspace
-python createWorkspace.py --cardname combinedCard.dat --workspace $wsname --rmin 0 --rmax 6 --processes prompt_WZ --processes prompt_ZZ --outpath $outpath
+python createWorkspace.py --cardname combinedCard.dat --workspace $wsname.root --rmin 0 --rmax 6 --processes prompt_WZ --processes prompt_ZZ --outpath $outpath #--pretend
 
 
 echo ===================
 echo Running impacts 
 echo ===================
-python runImpacts.py --workspace $wsname --POIs r_prompt_WZ --POIs r_prompt_ZZ --signalPOI r_prompt_WZ --ncores 12 --outpath $outpath
+python runImpacts.py --workspace $wsname.root --POIs r_prompt_WZ --POIs r_prompt_ZZ --signalPOI r_prompt_WZ --ncores 12 --outpath $outpath
