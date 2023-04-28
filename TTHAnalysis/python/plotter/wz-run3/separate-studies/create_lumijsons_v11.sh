9ea57c1b169241b93d3453bbc843a99a92b9f6fd
Carlos Vico Villalba <carlos.vico.villalba@cern.ch>
2023-04-28 12:49:27 +0200

    Stuff for computing luminosity with v11

---
+++ TTHAnalysis/python/plotter/wz-run3/separate-studies/create_lumijsons_v11.sh
@@ -0,0 +1,17 @@
+inpath="/beegfs/data/nanoAODv11/30march2023/data/2022PostEE/"
+golden="Cert_Collisions2022_355100_362760_Golden.json" # ERAS CDEFG
+
+## Muon ##
+python3 buildJSONfromNano.py --datasets Muon --inpath $inpath --filterjson $golden --filter-by-era Run2022F --filter-by-version v1-v2
+python3 buildJSONfromNano.py --datasets Muon --inpath $inpath --filterjson $golden --filter-by-era Run2022G --filter-by-version v1-v2
+
+
+## EGamma ##
+python3 buildJSONfromNano.py --datasets EGamma --inpath $inpath --filterjson $golden --filter-by-era Run2022F --filter-by-version v1-v2
+python3 buildJSONfromNano.py --datasets EGamma --inpath $inpath --filterjson $golden --filter-by-era Run2022G --filter-by-version v1-v2
+
+
+## MuonEG ##
+python3 buildJSONfromNano.py --datasets MuonEG --inpath $inpath --filterjson $golden --filter-by-era Run2022F --filter-by-version v1-v2
+python3 buildJSONfromNano.py --datasets MuonEG --inpath $inpath --filterjson $golden --filter-by-era Run2022G --filter-by-version v1-v2
+
