2380f97367796ea1b6a21f3446006d597de814eb
Carlos Vico Villalba <carlos.vico.villalba@cern.ch>
2023-06-10 12:36:50 +0200

    A lot of config files

--- TTHAnalysis/python/plotter/wz-run3/cfgs/Run3datasets_data_v11_postEE.py
+++ TTHAnalysis/python/plotter/wz-run3/cfgs/Run3datasets_data_v11_postEE.py
@@ -1,17 +1,26 @@
 """ Run 3 dataset names for nanoAODv10 production (DATA) """
-datasets = {"data" : {
+datasets = {
   # ---------------- Muon 
-  "Muon"       : [("Muon_Run2022F", ".*_Muon_Run2022F.*"),
-                  ("Muon_Run2022G", ".*_Muon_Run2022G.*")],
+  "Muon"       : [
+    ("Muon_Run2022F", ".*_Muon_Run2022F.*"),
+    ("Muon_Run2022G", ".*_Muon_Run2022G.*")
+  ],
   
   # ---------------- EGamma 
-  "EGamma"     : [("EGamma_Run2022F", ".*EGamma_Run2022F.*"),
-                  ("EGamma_Run2022G", ".*EGamma_Run2022G.*")],
+  "EGamma"     : [
+    ("EGamma_Run2022F", ".*EGamma_Run2022F.*"),
+    ("EGamma_Run2022G", ".*EGamma_Run2022G.*")
+  ],
 
   # ---------------- MuonEG 
   "MuonEG"     : [
-                  ("MuonEG_Run2022F", ".*MuonEG_Run2022F.*"),
-                  ("MuonEG_Run2022G", ".*MuonEG_Run2022G.*")
-                  ],
- }
+    ("MuonEG_Run2022F", ".*MuonEG_Run2022F.*"),
+    ("MuonEG_Run2022G", ".*MuonEG_Run2022G.*")
+  ],
+  # ---------------- JetMET 
+
+  "JetMET"     : [
+    ("JetMET_Run2022F", ".*JetMET_Run2022F.*"),
+    ("JetMET_Run2022G", ".*JetMET_Run2022G.*")
+  ]
 }
