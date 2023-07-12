f0de2a6c39bd667318e7673f696e114398726f62
raulblazquezbullon <raulblazquezbullon@gmail.com>
2023-07-12 17:20:06 +0200

    Changed name of impacts

---
+++ TTHAnalysis/python/plotter/check_discriminant_vars/cards_cut/rebin9/m3lmet_Meas/flav_mme/cards/slurm_combine_task.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+ulimit -s unlimited
+set -e
+cd /mnt_pool/c3_users/user/rblazquez/TFG/wz_run3/CMSSW_12_4_12/src
+export SCRAM_ARCH=slc7_amd64_gcc10
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+eval `scramv1 runtime -sh`
+cd /nfs/fanae/user/rblazquez/TFG/wz_run3/CMSSW_12_4_12/src/CMGTools/TTHAnalysis/python/plotter/check_discriminant_vars/cards_cut/rebin9/m3lmet_Meas/flav_mme/cards
+if [ ${SLURM_ARRAY_TASK_ID} -eq 1 ]; then
+  combine --algo grid --rMin 0 --rMax 6 higgsCombinebestfit.MultiDimFit.mH125.root --freezeParameters allConstrainedNuisances --snapshotName MultiDimFit --redefineSignalPOI r_prompt_WZ -M MultiDimFit -m 125 --points 200 -n _stat
+fi
+
+
--- TTHAnalysis/python/plotter/scanCards.py
+++ TTHAnalysis/python/plotter/scanCards.py
@@ -22,6 +22,7 @@ def add_parsing_options():
     parser.add_argument("--variables", dest = "variables", default = functional_variables.keys())
     parser.add_argument("--inpath",   dest = "inpath",default = "")
     parser.add_argument("--blind",    dest = "blind",action = "store_true")
+    parser.add_argument("--card_cut",    dest = "card_cut",default = "")
 
     return parser.parse_args() 
     
@@ -33,6 +34,7 @@ if __name__ == "__main__":
     inpath = opts.inpath
     blind = opts.blind
     doBlind = "-t -1 --setParameters r_prompt_WZ=1,r_prompt_ZZ=1" if blind else ""
+    card_cut  = opts.card_cut.split(",")
     
     seconds = 30
     check_d_vars = True
@@ -48,49 +50,54 @@ if __name__ == "__main__":
         for nq in nquant:
             
             print(">>> Going to rebin%s" %nq)
-            if check_d_vars == True:
-                inpath = main_path + "./check_discriminant_vars/./rebin%s/%s/cards/" %(nq,var)
                 
-            print("Path is" + inpath)
+            for cut_var in card_cut:
+                if check_d_vars == True:
+                    inpath_new = main_path + "./check_discriminant_vars/./rebin%s/%s/cards/" %(nq,var)
+                
+                else:
+                    if "" not in card_cut: inpath_new = main_path + inpath + "rebin%s/%s/%s/cards/" %(nq,var,cut_var)
+                    else: inpath_new = main_path + inpath + "%s/" %var
+                
             
-            print("==== Initializing scan ====")
-            print(">>> Combining cards")
-            cmd1 = "cd %s; combineCards.py *.txt > combinedCard.dat; cd -" %inpath
-            out1 = subprocess.check_output(cmd1,shell = True).decode("utf-8")
-            print(out1)
-            
-            print(">>> Creating workspace")
-            cmd2 = "cd %s; text2workspace.py combinedCard.dat -o workspace.root -P HiggsAnalysis.CombinedLimit.PhysicsModel:multiSignalModel --PO verbose --PO  'map=.*/prompt_WZ.*:r_prompt_WZ[1,0,6]' --PO 'map=.*/prompt_ZZ.*:r_prompt_ZZ[1,0,6]'; cd -" %inpath
-            out2 = subprocess.check_output(cmd2,shell = True).decode("utf-8")
-            print(out2)
-            
-            print(">>> Performing scan")
-            cmd3 = "cd %s; combineTool.py -M MultiDimFit --algo grid --points 200 --rMin 0 --rMax 6 -m 125 -d workspace.root %s --redefineSignalPOI r_prompt_WZ -n nominal --job-mode slurm --sub-opts='-p batch'; cd -" %(inpath,doBlind)
-            print("Running the following command: " + cmd3)
-            out3 = subprocess.check_output(cmd3,shell = True).decode("utf-8")
-            print(out3)
-            
-            print("Sleeping for %i seconds" %seconds)
-            time.sleep(seconds)
-            
-            print(">>> Saving snapshot")
-            cmd4 = "cd %s; combineTool.py -M MultiDimFit --algo none --rMin 0 --rMax 6 -m 125 -d workspace.root %s --redefineSignalPOI r_prompt_WZ -n bestfit --saveWorkspace; cd -" %(inpath,doBlind)
-            print("Running the following command: " + cmd4)
-            out4 = subprocess.check_output(cmd4,shell = True).decode("utf-8")
-            print(out4)
-            
-            print(">>> Finding statistic")
-            cmd5 = "cd %s; combineTool.py -M MultiDimFit --algo grid --points 200 --rMin 0 --rMax 6 -m 125 higgsCombinebestfit.MultiDimFit.mH125.root %s --freezeParameters allConstrainedNuisances --snapshotName MultiDimFit --redefineSignalPOI r_prompt_WZ -n _stat --job-mode slurm --sub-opts='-p batch'; cd -" %(inpath,doBlind)
-            print("Running the following command: " + cmd5)
-            out5 = subprocess.check_output(cmd5,shell = True).decode("utf-8")
-            print(out5)
-            
-            print(">>> Sleeping for %i seconds\n" %seconds)
-            time.sleep(seconds)
-            
-            print(">>> Plotting scan")
-            cmd6 = "cd %s; plot1DScan.py higgsCombinenominal.MultiDimFit.mH125.root --POI r_prompt_WZ  --others 'higgsCombine_stat.MultiDimFit.mH125.root:Stat-only:2'  --breakdown Syst,Stat --logo-sub Academic; cd -" %inpath
-            out6 = subprocess.check_output(cmd6,shell = True).decode("utf-8")
-            print(out6)
-            
-            print("==== Ending scan ====")
+                print("==== Initializing scan ====")
+                print(">>> Combining cards")
+                cmd1 = "cd %s; combineCards.py *.txt > combinedCard.dat; cd -" %inpath_new
+                out1 = subprocess.check_output(cmd1,shell = True).decode("utf-8")
+                print(out1)
+                
+                print(">>> Creating workspace")
+                cmd2 = "cd %s; text2workspace.py combinedCard.dat -o workspace.root -P HiggsAnalysis.CombinedLimit.PhysicsModel:multiSignalModel --PO verbose --PO  'map=.*/prompt_WZ.*:r_prompt_WZ[1,0,6]' --PO 'map=.*/prompt_ZZ.*:r_prompt_ZZ[1,0,6]'; cd -" %inpath_new
+                out2 = subprocess.check_output(cmd2,shell = True).decode("utf-8")
+                print(out2)
+                
+                print(">>> Performing scan")
+                cmd3 = "cd %s; combineTool.py -M MultiDimFit --algo grid --points 200 --rMin 0 --rMax 6 -m 125 -d workspace.root %s --redefineSignalPOI r_prompt_WZ -n nominal --job-mode slurm --sub-opts='-p batch'; cd -" %(inpath_new,doBlind)
+                print("Running the following command: " + cmd3)
+                out3 = subprocess.check_output(cmd3,shell = True).decode("utf-8")
+                print(out3)
+                
+                print("Sleeping for %i seconds" %seconds)
+                time.sleep(seconds)
+                
+                print(">>> Saving snapshot")
+                cmd4 = "cd %s; combineTool.py -M MultiDimFit --algo none --rMin 0 --rMax 6 -m 125 -d workspace.root %s --redefineSignalPOI r_prompt_WZ -n bestfit --saveWorkspace; cd -" %(inpath_new,doBlind)
+                print("Running the following command: " + cmd4)
+                out4 = subprocess.check_output(cmd4,shell = True).decode("utf-8")
+                print(out4)
+                
+                print(">>> Finding statistic")
+                cmd5 = "cd %s; combineTool.py -M MultiDimFit --algo grid --points 200 --rMin 0 --rMax 6 -m 125 higgsCombinebestfit.MultiDimFit.mH125.root %s --freezeParameters allConstrainedNuisances --snapshotName MultiDimFit --redefineSignalPOI r_prompt_WZ -n _stat --job-mode slurm --sub-opts='-p batch'; cd -" %(inpath_new,doBlind)
+                print("Running the following command: " + cmd5)
+                out5 = subprocess.check_output(cmd5,shell = True).decode("utf-8")
+                print(out5)
+                
+                print(">>> Sleeping for %i seconds\n" %seconds)
+                time.sleep(seconds)
+                
+                print(">>> Plotting scan")
+                cmd6 = "cd %s; plot1DScan.py higgsCombinenominal.MultiDimFit.mH125.root --POI r_prompt_WZ  --others 'higgsCombine_stat.MultiDimFit.mH125.root:Stat-only:2'  --breakdown Syst,Stat --logo-sub Academic; cd -" %inpath_new
+                out6 = subprocess.check_output(cmd6,shell = True).decode("utf-8")
+                print(out6)
+                
+                print("==== Ending scan ====")
